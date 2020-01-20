79533da15f2da305d1278dbbfbc655a0089ba540
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-01-20 17:10:37 -0500

    fixes filename

--- examples/pipeline/intermediate/util.py
+++ examples/pipeline/intermediate/util.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 def load_db_uri():
     try:
-        p = Path('~', '.auth', 'postgres-dstools.json').expanduser()
+        p = Path('~', '.auth', 'postgres-ploomber.json').expanduser()
 
         with open(p) as f:
             db = json.load(f)
