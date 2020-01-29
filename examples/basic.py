827917c134a45a35d48c33c2f08f74ddd3a1dd58
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-01-28 23:37:50 -0500

    finishes basic example

--- examples/basic.py
+++ examples/basic.py
@@ -26,19 +26,24 @@ from ploomber.products import File
 dag = DAG(name='my pipeline')
 
 
-# Let's now build the first tasks, they will just download some data, this pipeline is entirely declared in a single file to simplify things, a real pipeline will likely be splitted among several files.
-#
-# Tasks can be a lot of things (bash scripts, SQL scripts, etc), for this example they will be Python functions.
+# Let's now build the first tasks, they will just download some data, this
+# pipeline is entirely declared in a single file to simplify things, a real
+# pipeline will likely be splitted among several files.
 
-# +
+# Tasks can be a lot of things (bash scripts, SQL scripts, etc), for this
+# example they will be Python functions.
+
 # these function pull the data and save it, the product
 # parameter is required in every Task
 
 def get_red_wine_data(product):
     """Get red wine data
     """
-    df = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv',
-                     sep=';', index_col=False)
+    url = ('http://archive.ics.uci.edu/ml/machine-learning-databases/'
+           'wine-quality/winequality-red.csv')
+    df = pd.read_csv(url,
+                     sep=';',
+                     index_col=False)
     # producg is a File type so you have to cast it to a str
     df.to_csv(str(product))
 
@@ -46,8 +51,11 @@ def get_red_wine_data(product):
 def get_white_wine_data(product):
     """Get white wine data
     """
-    df = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv',
-                     sep=';', index_col=False)
+    url = ('http://archive.ics.uci.edu/ml/machine-learning-databases/'
+           'wine-quality/winequality-white.csv')
+    df = pd.read_csv(url,
+                     sep=';',
+                     index_col=False)
     df.to_csv(str(product))
 
 
@@ -62,7 +70,6 @@ def concat_data(upstream, product):
     df.to_csv(str(product))
 
 
-# +
 # create a temporary directory to store data
 tmp_dir = Path(tempfile.mkdtemp())
 
@@ -84,7 +91,6 @@ concat_task = PythonCallable(concat_data,
 # now we declare how our tasks relate to each other
 red_task >> concat_task
 white_task >> concat_task
-# -
 
 # we can plot our dag
 path_to_image = dag.plot(open_image=False)
@@ -93,21 +99,23 @@ display(Image(filename=path_to_image))
 # build the dag (execute all the tasks)
 dag.build()
 
-# Each time the DAG is run it will save the current timestamp and the source code of each task, next time we run it it will only run the necessary tasks to get everything up-to-date, there is a simple rule to that: a task will run if its code (or the code from any dependency) has changed since the last time it ran.
+# Each time the DAG is run it will save the current timestamp and the
+# source code of each task, next time we run it it will only run the
+# necessary tasks to get everything up-to-date, there is a simple rule to
+# that: a task will run if its code (or the code from any dependency) has
+# changed since the last time it ran.
 
-# if we build it again, nothing will run
-dag.build()
 
 # the plot will show which tasks are up-to-date
 # in green
-path_to_image = dag.plot(open_image=False)
+path_to_image = dag.plot(open_image=False, clear_cached_status=True)
 display(Image(filename=path_to_image))
 
 # status returns a summary of each task status
 dag.status()
 
 # ## Inspecting the `DAG` object
-#
+
 # The DAG object has utilities to debug and use the pipeline.
 
 # list all tasks in the dag
@@ -117,13 +125,17 @@ list(dag)
 task = dag['red']
 task
 
-# task plan returns the source code to be executed along with the input parameters and product
+# task plan returns the source code to be executed along with the input
+# parameters and product
 task.plan()
 
 # avoid hardcoding paths to files by loading them directly
 # from the DAG, casting a Task to a str, will cause them
 # to return a valid representation, in this case, our
-# product is a File, so it will return a path to it
+# product is a File, so it will return a path to it, for SQL relations
+# str(product) will return "schema"."name". Using this method for loading
+# products makes sure you don't have to hardcode paths to files and that
+# given your pipeline definition, you always read from the right place
 df = pd.read_csv(str(dag['red']))
 
 df.head()
