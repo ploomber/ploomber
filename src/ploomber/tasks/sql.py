4e0617cb2b9238de1fb97dd6ae9e7e4f387f414a
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-02-04 22:45:47 -0500

    adds SQLTransfer docstring

--- src/ploomber/tasks/sql.py
+++ src/ploomber/tasks/sql.py
@@ -8,6 +8,7 @@ from ploomber.sources import (SQLScriptSource,
                               GenericSource)
 from ploomber.products import File, PostgresRelation, SQLiteRelation
 from ploomber import io
+from ploomber.util import requires
 
 import pandas as pd
 
@@ -156,10 +157,45 @@ class SQLDump(Task):
 
 
 class SQLTransfer(Task):
-    """Transfers data from a SQL statement to a SQL relation
+    """
+    Transfers data from a SQL database to another (Note: this relies on
+    pandas methods, only use it for small to medium size datasets)
+
+    Parameters
+    ----------
+    source: str or pathlib.Path
+        SQL script source, if str, the content is interpreted as the actual
+        script, if pathlib.Path, the content of the file is loaded
+    product: ploomber.Product
+        Product generated upon successful execution. For SQLTransfer, usually
+        product.client != task.client. task.client represents the data source
+        while product.client represents the data destination.
+    dag: ploomber.DAG
+        A DAG to add this task to
+    name: str
+        A str to indentify this task. Should not already exist in the dag
+    client: ploomber.clients.DBAPIClient or SQLAlchemyClient, optional
+        The client used to connect to the database. Only required
+        if no dag-level client has been declared using dag.clients[class_name]
+    params: dict, optional
+        Parameters to pass to the script, by default, the callable will
+        be executed with a "product" (which will contain the product object).
+        It will also include a "upstream" parameter if the task has upstream
+        dependencies along with any parameters declared here. The source
+        code is converted to a jinja2.Template for passing parameters,
+        refer to jinja2 documentation for details
+    chunksize: int, optional
+        Number of rows to transfer on every chunk
+
+    Notes
+    ----
+    This task is *not* intended to be used to move large datasets, but a
+    convenience way of transfering small to medium size datasets. It relies
+    on pandas to read and write, which introduces a considerable overhead.
     """
     PRODUCT_CLASSES_ALLOWED = (PostgresRelation, SQLiteRelation)
 
+    @requires(['pandas'], 'SQLTransfer')
     def __init__(self, source, product, dag, name, client=None,
                  params=None, chunksize=10000):
         params = params or {}
@@ -195,6 +231,7 @@ class SQLTransfer(Task):
                       index=False)
 
 
+# FIXME: support other data formats apart from parquet
 class SQLUpload(Task):
     """Upload data to a database from a parquet file
 
