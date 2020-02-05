d512542ea51bae7b9a3f91743513ffe251b24de5
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-02-04 22:36:31 -0500

    adds SQLDump docstring

--- src/ploomber/tasks/sql.py
+++ src/ploomber/tasks/sql.py
@@ -26,9 +26,10 @@ class SQLScript(Task):
         A DAG to add this task to
     name: str
         A str to indentify this task. Should not already exist in the dag
-    client: ploomber.clients.DBAPIClient or SQLAlchemyClient
-        The client used to connect to the database
-    params: dict
+    client: ploomber.clients.DBAPIClient or SQLAlchemyClient, optional
+        The client used to connect to the database. Only required
+        if no dag-level client has been declared using dag.clients[class_name]
+    params: dict, optional
         Parameters to pass to the script, by default, the callable will
         be executed with a "product" (which will contain the product object).
         It will also include a "upstream" parameter if the task has upstream
@@ -57,43 +58,55 @@ class SQLScript(Task):
 
 
 class SQLDump(Task):
-    """
-    Dumps data from a SQL SELECT statement to parquet files (one per chunk)
+    """Dumps data from a SQL SELECT statement to a file(s)
 
     Parameters
     ----------
-    source: str
-        The SQL query to run in the database
-    product: ploomber.products.File
-        The directory location for the output files
-    dag: DAG
-        The DAG for this task
+    source: str or pathlib.Path
+        SQL script source, if str, the content is interpreted as the actual
+        script, if pathlib.Path, the content of the file is loaded
+    product: ploomber.Product
+        Product generated upon successful execution
+    dag: ploomber.DAG
+        A DAG to add this task to
     name: str
-        Name for this task
+        A str to indentify this task. Should not already exist in the dag
+    client: ploomber.clients.DBAPIClient or SQLAlchemyClient, optional
+        The client used to connect to the database. Only required
+        if no dag-level client has been declared using dag.clients[class_name]
     params: dict, optional
-        Extra parameters for the task
+        Parameters to pass to the script, by default, the callable will
+        be executed with a "product" (which will contain the product object).
+        It will also include a "upstream" parameter if the task has upstream
+        dependencies along with any parameters declared here. The source
+        code is converted to a jinja2.Template for passing parameters,
+        refer to jinja2 documentation for details
     chunksize: int, optional
-        Size of each chunk, one parquet file will be generated per chunk. If
-        None, only one file is created
-
+        Number of rows per file, otherwise download the entire dataset in
+        a single one. If set, the product will be a folder
+    io_handler: ploomber.io.CSVIO or ploomber.io.ParquetIO, optional
+        io handler to use (which controls the output format), currently
+        only csv and parquet are supported. Defaults to ploomber.io.CSVIO
 
     Notes
     -----
-    The chunksize parameter is set in cursor.arraysize object, this parameter
-    can greatly speed up the dump for some databases when the driver uses
-    cursors.arraysize as the number of rows to fetch on a single call
+    The chunksize parameter is also set in cursor.arraysize object, this
+    parameter can greatly speed up the dump for some databases when the
+    driver uses cursors.arraysize as the number of rows to fetch on a single
+    network trip, but this is driver-dependent, not all drivers implement
+    this (cx_Oracle does it)
     """
     PRODUCT_CLASSES_ALLOWED = (File, )
 
     def __init__(self, source, product, dag, name, client=None,
                  params=None,
-                 chunksize=10000, io_handler=None):
+                 chunksize=10000, io_handler=io.CSVIO):
         params = params or {}
         super().__init__(source, product, dag, name, params)
 
         self.client = client or self.dag.clients.get(type(self))
         self.chunksize = chunksize
-        self.io_handler = io_handler or io.CSVIO
+        self.io_handler = io_handler
 
         if self.client is None:
             raise ValueError('{} must be initialized with a client'
