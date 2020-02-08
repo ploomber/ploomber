9b551267ca5065cad0944a378ff75ea96db22b39
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-02-07 20:13:19 -0500

    validation improvements, adds validate_values

--- src/ploomber/validators/__init__.py
+++ src/ploomber/validators/__init__.py
@@ -1,3 +1,5 @@
-from ploomber.validators.validators import data_frame_validator
+from ploomber.validators.validators import (data_frame_validator,
+                                            validate_schema,
+                                            validate_values)
 
-__all__ = ['data_frame_validator']
+__all__ = ['data_frame_validator', 'validate_schema', 'validate_values']
