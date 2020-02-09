0e9895cf848451491a2246769212166fe5f4a2d5
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-02-09 10:52:58 -0500

    testing ShellClient with custom run_template

--- tests/pipeline/test_clients.py
+++ tests/pipeline/test_clients.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
+from ploomber import DAG
+from ploomber.tasks import ShellScript
+from ploomber.products import File
 from ploomber.clients import ShellClient
 
 
@@ -28,5 +31,29 @@ def test_shell_client_with_custom_template(tmp_directory):
     assert not path.exists()
 
     client.execute(code)
+
+    assert path.exists()
+
+
+def test_custom_client_in_dag(tmp_directory):
+    path = Path(tmp_directory, 'a_file')
+
+    dag = DAG()
+
+    client = ShellClient(run_template='ruby {{path_to_code}}')
+
+    dag.clients[ShellScript] = client
+
+    ShellScript("""
+    require 'fileutils'
+    FileUtils.touch "{{product}}"
+    """,
+                product=File(path),
+                dag=dag,
+                name='ruby_script')
+
+    assert not path.exists()
+
+    dag.build()
 
     assert path.exists()
