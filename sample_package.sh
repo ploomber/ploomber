da17987f084d4506c131d0af807803d7259f97d0
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-02-12 18:19:38 -0500

    installing sample package for tests

---
+++ sample_package.sh
@@ -0,0 +1,9 @@
+# this is used in test_env.py
+wget https://github.com/edublancas/ds-template/archive/master.zip
+unzip master.zip
+cd ds-template-master/basic-template/
+mv 'src/{{project_name}}' 'src/sample_project'
+sed 's/{{package_name}}/sample_project/g' setup.py > setup-tmp.py
+rm -f setup.py
+mv setup-tmp.py setup.py
+pip install .
