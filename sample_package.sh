38ddecf15d3db40ed7b104c4a89c542c3dc49a0f
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-02-29 07:05:30 -0500

    adds missing lines that install sample project

--- sample_package.sh
+++ sample_package.sh
@@ -3,3 +3,5 @@ curl -O -L https://github.com/ploomber/template/archive/master.zip
 unzip master.zip
 bash template-master/install.sh sample_project
 rm -f master.zip
+pip install sample_project/
+rm -rf sample_project/
