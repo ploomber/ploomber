214dc1514df5a4f25c558e27f445345e28b62ae6
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-02-29 07:24:20 -0500

    fixing import

--- src/ploomber/util/__init__.py
+++ src/ploomber/util/__init__.py
@@ -1,6 +1,7 @@
 from ploomber.util.util import (safe_remove, image_bytes2html,
                                 clean_up_files, isiterable, path2fig,
-                                requires, markup)
+                                requires)
+from ploomber.util import markup
 from ploomber.util.param_grid import Interval, ParamGrid
 
 __all__ = ['safe_remove', 'image_bytes2html', 'clean_up_files', 'Interval',
