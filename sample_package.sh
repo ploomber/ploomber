87799abe603fed54b2380ac45c7013e685dd410d
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-02-29 06:56:48 -0500

    fixing sample project name

--- sample_package.sh
+++ sample_package.sh
@@ -1,5 +1,5 @@
 # this is used in test_env.py
 curl -O -L https://github.com/ploomber/template/archive/master.zip
 unzip master.zip
-bash template-master/install.sh sample_package
+bash template-master/install.sh sample_project
 rm -f master.zip
