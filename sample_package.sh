c6b55612986ba5518965ec242873398c8179edbe
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-02-29 06:56:04 -0500

    updates instructions for installing sample package

--- sample_package.sh
+++ sample_package.sh
@@ -1,11 +1,5 @@
 # this is used in test_env.py
-wget https://github.com/edublancas/ds-template/archive/master.zip
+curl -O -L https://github.com/ploomber/template/archive/master.zip
 unzip master.zip
-cd ds-template-master/basic-template/
-mv 'src/{{package_name}}' 'src/sample_project'
-sed 's/{{package_name}}/sample_project/g' setup.py > setup-tmp.py
-rm -f setup.py
-mv setup-tmp.py setup.py
-pip install .
-cd ../..
-rm -rf ds-template-master
+bash template-master/install.sh sample_package
+rm -f master.zip
