a61b7207457ec9d5c3e51c417b24be62e10e3321
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-03-07 11:16:25 -0500

    closes #11, closes #12

--- tests/test_clients.py
+++ tests/test_clients.py
@@ -1,9 +1,36 @@
+import sqlite3
+import pickle
+import copy
 from pathlib import Path
 
 from ploomber import DAG
 from ploomber.tasks import ShellScript
 from ploomber.products import File
-from ploomber.clients import ShellClient
+from ploomber.clients import ShellClient, SQLAlchemyClient, DBAPIClient
+
+
+def test_deepcopy_dbapiclient(tmp_directory):
+    client = DBAPIClient(sqlite3.connect, database='my_db.db')
+    client.execute('CREATE TABLE my_table (num INT)')
+    assert copy.deepcopy(client)
+
+
+def test_pickle_dbapiclient(tmp_directory):
+    client = DBAPIClient(sqlite3.connect, database='my_db.db')
+    client.execute('CREATE TABLE my_table (num INT)')
+    assert pickle.dumps(client)
+
+
+def test_deepcopy_sqlalchemyclient(tmp_directory):
+    client = SQLAlchemyClient('sqlite:///my_db.db')
+    client.execute('CREATE TABLE my_table (num INT)')
+    assert copy.deepcopy(client)
+
+
+def test_pickle_sqlalchemyclient(tmp_directory):
+    client = SQLAlchemyClient('sqlite:///my_db.db')
+    client.execute('CREATE TABLE my_table (num INT)')
+    assert pickle.dumps(client)
 
 
 def test_shell_client(tmp_directory):
