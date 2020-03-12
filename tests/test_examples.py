de7f23e530fe33a1dbe438caf2ec44d1264a222e
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-03-12 18:24:15 -0400

    testing all examples in the examples folder

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,7 +1,14 @@
 """
 Runs examples form examples/
 """
+import os
+import pytest
 import subprocess
+from conftest import _path_to_tests
+
+
+_examples = [f for f in os.listdir(_path_to_tests().parent / 'examples')
+             if f.endswith('.py')]
 
 
 def test_pipeline_runs(tmp_intermediate_example_directory):
@@ -10,3 +17,9 @@ def test_pipeline_runs(tmp_intermediate_example_directory):
 
 def test_partitioned_execution_runs(tmp_example_pipeline_directory):
     assert subprocess.call(['python', 'partitioned_execution.py']) == 0
+
+
+@pytest.mark.parametrize('name', _examples)
+def test_examples(tmp_examples_directory, name):
+    # TODO: add timeout
+    assert subprocess.call(['python', name]) == 0
