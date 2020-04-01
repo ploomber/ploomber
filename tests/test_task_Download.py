8bb08ed46b1b4eb741af0688afb4f88ebe3e712d
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-04-01 11:00:24 -0400

    changing test url, getting certificate error in UCI repository

--- tests/test_task_Download.py
+++ tests/test_task_Download.py
@@ -7,8 +7,8 @@ def test_can_download_file(tmp_directory):
     dag = DAG()
 
     url = """
-    https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data
+    https://github.com
     """
-    DownloadFromURL(url, File('iris.data'), dag=dag, name='download')
+    DownloadFromURL(url, File('file'), dag=dag, name='download')
 
     assert dag.build()
