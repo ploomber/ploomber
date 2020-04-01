9ca543edcf2c9ec86f5c4b0b7b4b4ab7525513f0
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-04-01 09:38:55 -0400

    fixing template installation

--- sample_package.sh
+++ sample_package.sh
@@ -1,7 +1,10 @@
 # this is used in test_env.py
+mkdir tmp/
+cd tmp
 curl -O -L https://github.com/ploomber/template/archive/master.zip
 unzip master.zip
-bash template-master/install.sh sample_project
+python template-master/install.py --name sample_project
 rm -f master.zip
-pip install sample_project/
-rm -rf sample_project/
+pip install .
+cd ..
+rm -rf tmp/
