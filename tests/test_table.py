13f12738d46624353df7d0dd0ad0f52ca1630c49
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-04-04 18:01:59 -0400

    fixing test for python 3.5

--- tests/test_table.py
+++ tests/test_table.py
@@ -3,10 +3,14 @@ from ploomber.Table import Row, Table
 
 def test_row():
     r = Row({'a': 1, 'b': 2})
-    assert str(r) == '  a    b\n---  ---\n  1    2'
+    assert (str(r) == '  a    b\n---  ---\n  1    2'
+            # python 3.5 does not guarantee order
+            or str(r) == '  b    a\n---  ---\n  2    1')
 
 
 def test_table():
     r = Row({'a': 1, 'b': 2})
     t = Table([r, r])
-    assert str(t) == '  a    b\n---  ---\n  1    2\n  1    2'
+    assert (str(t) == '  a    b\n---  ---\n  1    2\n  1    2'
+            # python 3.5 does not guarantee order
+            or str(t) == '  b    a\n---  ---\n  2    1\n  2    1')
