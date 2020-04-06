2dd927f44f2618830d0006cf8ffd18c1c439727c
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-04-06 09:09:01 -0400

    fixing package installation

--- sample_package.sh
+++ sample_package.sh
@@ -3,8 +3,8 @@ mkdir tmp/
 cd tmp
 curl -O -L https://github.com/ploomber/template/archive/master.zip
 unzip master.zip
-python template-master/install.py --name sample_project
 rm -f master.zip
+python template-master/install.py --name sample_project
 pip install .
 cd ..
 rm -rf tmp/
