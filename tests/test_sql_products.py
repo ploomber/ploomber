3a4a0d3b8571637e39f8f00dda5c948eed36b367
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-05-13 19:12:00 -0400

    passing missing schema for pg tests

--- tests/test_sql_products.py
+++ tests/test_sql_products.py
@@ -16,11 +16,12 @@ def client_and_prod(request, sqlite_client_and_tmp_dir, pg_client_and_schema):
     if request.param == 'sqlite':
         client, _ = sqlite_client_and_tmp_dir
         product = SQLiteRelation((None, 'numbers', 'table'), client)
+        schema = None
     else:
         client, schema = pg_client_and_schema
         product = PostgresRelation((schema, 'numbers', 'table'), client)
 
-    yield client, product
+    yield client, product, schema
 
     product.delete()
 
@@ -31,23 +32,22 @@ def add_number_one(metadata):
 
 
 def test_exists(client_and_prod):
-    client, product = client_and_prod
+    client, product, schema = client_and_prod
     product.render({})
     product.delete()
 
     assert not product.exists()
 
     df = pd.DataFrame({'a': np.arange(0, 100), 'b': np.arange(100, 200)})
-    df.to_sql('numbers', client.engine, if_exists='replace')
+    df.to_sql('numbers', client.engine, if_exists='replace', schema=schema)
 
     assert product.exists()
 
 
 def test_delete(client_and_prod):
-    client, product = client_and_prod
-
+    client, product, schema = client_and_prod
     df = pd.DataFrame({'a': np.arange(0, 100), 'b': np.arange(100, 200)})
-    df.to_sql('numbers', client.engine, if_exists='replace')
+    df.to_sql('numbers', client.engine, if_exists='replace', schema=schema)
 
     product.render({})
     product.delete()
@@ -56,26 +56,25 @@ def test_delete(client_and_prod):
 
 
 def test_fetch_metadata_none_if_not_exists(client_and_prod):
-    client, product = client_and_prod
+    client, product, schema = client_and_prod
     product.render({})
 
     assert product.fetch_metadata() is None
 
 
 def test_fetch_metadata_none_if_empty_metadata(client_and_prod):
-    client, product = client_and_prod
+    client, product, schema = client_and_prod
     df = pd.DataFrame({'a': np.arange(0, 100), 'b': np.arange(100, 200)})
-    df.to_sql('numbers', client.engine, if_exists='replace')
+    df.to_sql('numbers', client.engine, if_exists='replace', schema=schema)
     product.render({})
 
     assert product.fetch_metadata() is None
 
 
 def test_save_metadata(client_and_prod):
-    client, product = client_and_prod
-
+    client, product, schema = client_and_prod
     df = pd.DataFrame({'a': np.arange(0, 100), 'b': np.arange(100, 200)})
-    df.to_sql('numbers', client.engine, if_exists='replace')
+    df.to_sql('numbers', client.engine, if_exists='replace', schema=schema)
 
     product.render({})
     metadata_new = {'timestamp': datetime.now().timestamp(),
@@ -89,7 +88,7 @@ def test_save_metadata(client_and_prod):
 
 
 def test_add_metadata_fields(client_and_prod):
-    client, product = client_and_prod
+    client, product, schema = client_and_prod
     dag = DAG()
     dag.clients[SQLScript] = client
     dag.clients[type(product)] = client
