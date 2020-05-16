3b26c1e533cb5e73bdae338a23cb2b3446d8e041
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-05-16 01:32:42 -0400

    fixing test_pkg setup

--- test_pkg.sh
+++ test_pkg.sh
@@ -1,2 +1,4 @@
- cd tests/assets/test_pkg
- pip install .
+cd tests/assets/test_pkg
+# install inplace as some tests make assumption on the folder structure
+# to create backups
+pip install --editable .
