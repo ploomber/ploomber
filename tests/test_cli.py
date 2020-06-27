b6c48b17e30017ab6addc6cab00f6ea6636972e3
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-06-27 15:38:20 -0400

    fixes test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 import subprocess
 import click
 from ploomber.cli import _new
@@ -7,5 +6,4 @@ from ploomber.cli import _new
 def test_ploomber_new(tmp_directory, monkeypatch):
     monkeypatch.setattr(click, 'confirm', lambda x: True)
     _new()
-    Path('output').mkdir()
     assert not subprocess.call(['ploomber', 'entry', 'pipeline.yaml'])
