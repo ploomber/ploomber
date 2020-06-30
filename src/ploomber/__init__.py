58313b78375c3028ab710bb4295a534d99a4733c
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-06-30 11:44:36 -0400

    Release 0.5.1

--- src/ploomber/__init__.py
+++ src/ploomber/__init__.py
@@ -5,7 +5,7 @@ from ploomber.env.decorators import load_env, with_env
 from ploomber.placeholders.SourceLoader import SourceLoader
 
 
-__version__ = '0.6dev'
+__version__ = '0.5.1'
 
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
