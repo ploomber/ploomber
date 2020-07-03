e6caaac3403358fd789a2767816ded5b9f33e21d
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-07-03 18:29:30 -0400

    adds tests for jupyter contents manager

---
+++ tests/assets/nbs/dummy.py
@@ -0,0 +1,14 @@
+"""
+This is a dummy file whose only purpose is to check that the custom jupyter
+contents manager does not inject a cell here since this is not part of
+the pipeline.yaml
+"""
+# + tags=["parameters"]
+upstream = {'clean': None}
+product = 'output/plot.ipynb'
+
+# +
+1 + 1
+
+# +
+2 + 2
---
+++ tests/test_jupyter.py
@@ -0,0 +1,69 @@
+"""
+Tests for the custom jupyter contents manager
+"""
+from ipython_genutils.tempdir import TemporaryDirectory
+from notebook.services.contents.tests.test_manager import TestContentsManager
+import jupytext
+
+from ploomber.jupyter import PloomberContentsManager
+
+
+class PloomberContentsManagerTestCase(TestContentsManager):
+    """
+    This runs the original test suite from jupyter to make sure our
+    content manager works ok
+
+    https://github.com/jupyter/notebook/blob/b152dd314decda6edbaee1756bb6f6fc50c50f9f/notebook/services/contents/tests/test_manager.py#L218
+
+    Docs: https://jupyter-notebook.readthedocs.io/en/stable/extending/contents.html#testing
+    """
+
+    def setUp(self):
+        self._temp_dir = TemporaryDirectory()
+        self.td = self._temp_dir.name
+        self.contents_manager = PloomberContentsManager(root_dir=self.td)
+
+
+# the following tests check our custom logic
+
+def get_injected_cell(nb):
+    injected = None
+
+    for cell in nb['cells']:
+        if 'injected-parameters' in cell['metadata'].get('tags', []):
+            injected = cell
+
+    return injected
+
+
+def test_injects_cell_if_file_in_dag(tmp_nbs):
+    cm = PloomberContentsManager()
+    model = cm.get('plot.py')
+
+    injected = get_injected_cell(model['content'])
+
+    assert injected
+    expected = ('# Parameters\nupstream = '
+                '{"clean": {"nb": "output/clean.ipynb", "data": '
+                '"output/clean.csv"}}\nproduct = '
+                '"output/plot.ipynb"\n')
+    assert expected == injected['source']
+
+
+def test_removes_injected_cell(tmp_nbs):
+    cm = PloomberContentsManager()
+    model = cm.get('plot.py')
+    cm.save(model, path='/plot.py')
+
+    nb = jupytext.read('plot.py')
+    assert get_injected_cell(nb) is None
+
+
+def test_skips_if_file_not_in_dag(tmp_nbs):
+    cm = PloomberContentsManager()
+    model = cm.get('dummy.py')
+    nb = jupytext.read('dummy.py')
+
+    # this file is not part of the pipeline, the contents manager should not
+    # inject cells
+    assert len(model['content']['cells']) == len(nb.cells)
