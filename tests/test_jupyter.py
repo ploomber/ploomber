725fe9d04ca4eaa7bef44aa115033351fe171936
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-07-06 21:21:48 -0400

    testing jupyter hot reload

--- tests/test_jupyter.py
+++ tests/test_jupyter.py
@@ -4,6 +4,7 @@ Tests for the custom jupyter contents manager
 import os
 from pathlib import Path
 
+import yaml
 from ipython_genutils.tempdir import TemporaryDirectory
 from notebook.services.contents.tests.test_manager import TestContentsManager
 import jupytext
@@ -86,7 +87,7 @@ def test_skips_if_file_not_in_dag(tmp_nbs):
 
 def test_import(tmp_nbs):
     # make sure we are able to import modules in the current working
-    # dicrectory
+    # directory
     Path('pipeline.yaml').unlink()
     os.rename('pipeline-w-location.yaml', 'pipeline.yaml')
     PloomberContentsManager()
@@ -102,3 +103,38 @@ def test_injects_cell_when_initialized_from_sub_directory(tmp_nbs_nested):
 
     injected = get_injected_cell(model['content'])
     assert injected
+
+
+def test_hot_reload(tmp_nbs):
+    # modify base pipeline.yaml to enable hot reload
+    with open('pipeline.yaml') as f:
+        spec = yaml.load(f, Loader=yaml.SafeLoader)
+
+    spec['meta']['jupyter_hot_reload'] = True
+    spec['meta']['extract_upstream'] = True
+
+    for t in spec['tasks']:
+        t.pop('upstream', None)
+
+    with open('pipeline.yaml', 'w') as f:
+        yaml.dump(spec, f)
+
+    cm = PloomberContentsManager()
+
+    model = cm.get('plot.py')
+    assert get_injected_cell(model['content'])
+
+    # replace upstream with a task that does not exist
+    path = Path('plot.py')
+    original_code = path.read_text()
+    new_code = original_code.replace("{'clean': None}", "{'no_task': None}")
+    path.write_text(new_code)
+
+    # not cell should be injected now
+    model = cm.get('plot.py')
+    assert not get_injected_cell(model['content'])
+
+    # fix it
+    path.write_text(original_code)
+    model = cm.get('plot.py')
+    assert get_injected_cell(model['content'])
