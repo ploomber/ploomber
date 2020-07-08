e18671653aaacd21a59a64ecbd96e3e13510e942
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-07-08 02:12:56 -0400

    closes #178

--- src/ploomber/spec/__init__.py
+++ src/ploomber/spec/__init__.py
@@ -3,6 +3,11 @@ Ploomber spec API provides a quick and simple way to use Ploomber using YAML,
 it is not indented to replace the Python API which offers more flexibility and
 advanced features.
 
+To understand the spec API you have to know a few basic concepts: "tasks" are
+scripts that generate "products" (which can be local files or tables/views in
+a database). If a task B has task A as an "upstream" dependency, it means B
+uses product(s) from A as inputs.
+
 `Click here for a live demo. <https://mybinder.org/v2/gh/ploomber/projects/master?filepath=spec%2FREADME.md>`_
 
 To create a new project with basic structure:
@@ -10,6 +15,13 @@ To create a new project with basic structure:
 .. code-block:: bash
 
     ploomber new
+
+To add tasks to an existing project:
+
+
+.. code-block:: bash
+
+    ploomber add my_new_task.py # or .sql
 
 
 Build pipeline:
