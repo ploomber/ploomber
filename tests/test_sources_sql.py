c1a0bc2284f7555328a944338c53ac63078a3443
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-07-16 00:58:57 -0400

    fixes test

--- tests/test_sources_sql.py
+++ tests/test_sources_sql.py
@@ -1,9 +1,14 @@
 from ploomber.sources import SQLScriptSource
+from ploomber.tasks.Params import Params
 
-source = SQLScriptSource("""
+
+def test_script_source():
+    source = SQLScriptSource("""
 {% set product =  PostgresRelation(["schema", "name", "table"]) %}
 
 CREATE TABLE {{product}} AS
 SELECT * FROM some_table
 """)
-x = source.render({'product': 'something'})
+    params = Params()
+    params._dict['product'] = 'this should be ignored'
+    assert source.render(params)
