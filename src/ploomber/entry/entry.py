99d282a383c422fb0a655d5168e61f0d5fb57b3b
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-07-21 19:08:18 -0400

    cleanin up entry module

--- src/ploomber/entry/entry.py
+++ src/ploomber/entry/entry.py
@@ -126,6 +126,134 @@ def _parse_module(s):
     return '.'.join(parts[:-1]), parts[-1]
 
 
+def _parse_signature_from_callable(callable_):
+    """
+    Parse a callable signature, return a dictionary with
+    {param_key: default_value} and a list of required parameters
+    """
+    sig = inspect.signature(callable_)
+
+    required = [
+        k for k, v in sig.parameters.items() if v.default == inspect._empty
+    ]
+
+    defaults = {
+        k: v.default
+        for k, v in sig.parameters.items() if v.default != inspect._empty
+    }
+
+    return required, defaults
+
+
+def get_desc(doc, arg):
+    arg_data = doc['params'].get(arg)
+    return None if arg_data is None else arg_data['desc']
+
+
+def _add_args_from_callable(parser, callable_):
+    """
+    Modifies an args parser to include parameters from a callable, adding
+    parameters with default values as optional and parameters with no defaults
+    as mandatory. Adds descriptions from parsing the callable's docstring
+
+    Returns parsed args: required (list) and defaults (dict)
+    """
+    doc = _parse_doc(callable_.__doc__)
+    required, defaults = _parse_signature_from_callable(callable_)
+
+    for arg, default in defaults.items():
+        parser.add_argument('--' + arg, help=get_desc(doc, arg))
+
+    for arg in required:
+        parser.add_argument(arg, help=get_desc(doc, arg))
+
+    return required, defaults
+
+
+def _process_file_entry_point(parser, entry_point):
+    if Path('env.yaml').exists():
+        env_dict = EnvDict('env.yaml')
+        _add_args_from_env_dict(parser, env_dict)
+
+    args = parser.parse_args()
+
+    if args.log is not None:
+        logging.basicConfig(level=args.log)
+
+    with open(entry_point) as f:
+        dag_dict = yaml.load(f, Loader=yaml.SafeLoader)
+
+    if Path('env.yaml').exists():
+        env = EnvDict('env.yaml')
+        replaced = _args_to_replace_in_env(args)
+        env = env._replace_flatten_keys(replaced)
+        dag = DAGSpec(dag_dict, env=env).to_dag()
+    else:
+        dag = DAGSpec(dag_dict).to_dag()
+
+    getattr(dag, args.action)()
+    return dag
+
+
+def _process_factory_entry_point(parser, entry_point):
+    mod, name = _parse_module(entry_point)
+
+    try:
+        module = importlib.import_module(mod)
+    except ImportError as e:
+        raise ImportError('An error happened when trying to '
+                          'import module "{}"'.format(mod)) from e
+
+    try:
+        entry = getattr(module, name)
+    except AttributeError as e:
+        raise AttributeError('Could not get attribute "{}" from module '
+                             '"{}", make sure it is a valid callable'.format(
+                                 name, mod)) from e
+
+    required, _ = _add_args_from_callable(entry)
+
+    # if entry point was decorated with @with_env, add arguments
+    # to replace declared variables in env.yaml
+    if hasattr(entry, '_env_dict'):
+        _add_args_from_env_dict(parser, entry._env_dict)
+
+    args = parser.parse_args()
+
+    if args.log is not None:
+        logging.basicConfig(level=args.log)
+
+    # required by the function signature
+    kwargs = {key: getattr(args, key) for key in required}
+
+    # env and function defaults replaced
+    replaced = _args_to_replace_in_env(args)
+
+    # TODO: add a way of test this by the parameters it will use to
+    # call the function, have an aux function to get those then another
+    # to execute, test using the first one
+    dag = entry(**{**kwargs, **replaced})
+
+    print(getattr(dag, args.action)())
+
+    return dag
+
+
+def _process_entry_point(parser, entry_point):
+    """
+    Process an entry point (either a file or a dotted path to a factory)
+    """
+    # first check if the entry point is an existing file
+    if Path(entry_point).exists():
+        dag = _process_file_entry_point(parser, entry_point)
+
+    # assume it's a dotted path to a factory
+    else:
+        dag = _process_factory_entry_point(parser, entry_point)
+
+    return dag
+
+
 def _main():
     parser = argparse.ArgumentParser()
     parser.add_argument('entry_point', help='Entry point (DAG)')
@@ -138,105 +266,19 @@ def _main():
                         'build',
                         default='build')
 
+    # we add args dynamically based on positional arguments, we need
+    # to know how many we got
     n_positional = len([arg for arg in sys.argv if not arg.startswith('-')])
 
-    if n_positional < 2:
-        args = parser.parse_args()
+    # this happens when calling python -m ploomber.entry
+    if n_positional <= 1:
+        # just parse args, this is needed to make --help work
+        parser.parse_args()
+    # we got an actual positional argument parse entry_point
     else:
-        # parse entry_point
         entry_point = sys.argv[1]
-
-        if Path(entry_point).exists():
-
-            if Path('env.yaml').exists():
-                env_dict = EnvDict('env.yaml')
-                _add_args_from_env_dict(parser, env_dict)
-
-            args = parser.parse_args()
-
-            if args.log is not None:
-                logging.basicConfig(level=args.log)
-
-            with open(entry_point) as f:
-                dag_dict = yaml.load(f, Loader=yaml.SafeLoader)
-
-            if Path('env.yaml').exists():
-                env = EnvDict('env.yaml')
-                replaced = _args_to_replace_in_env(args)
-                env = env._replace_flatten_keys(replaced)
-                dag = DAGSpec(dag_dict, env=env).to_dag()
-            else:
-                dag = DAGSpec(dag_dict).to_dag()
-
-            getattr(dag, args.action)()
-            return dag
-
-        else:
-
-            mod, name = _parse_module(entry_point)
-
-            try:
-                module = importlib.import_module(mod)
-            except ImportError as e:
-                raise ImportError('An error happened when trying to '
-                                  'import module "{}"'.format(mod)) from e
-
-            try:
-                entry = getattr(module, name)
-            except AttributeError as e:
-                raise AttributeError(
-                    'Could not get attribute "{}" from module '
-                    '"{}", make sure it is a valid callable'.format(
-                        name, mod)) from e
-
-            doc = _parse_doc(entry.__doc__)
-
-            def get_desc(arg):
-                arg_data = doc['params'].get(arg)
-                return None if arg_data is None else arg_data['desc']
-
-            sig = inspect.signature(entry)
-
-            defaults = {
-                k: v.default
-                for k, v in sig.parameters.items()
-                if v.default != inspect._empty
-            }
-            required = [
-                k for k, v in sig.parameters.items()
-                if v.default == inspect._empty
-            ]
-
-            for arg, default in defaults.items():
-                parser.add_argument('--' + arg, help=get_desc(arg))
-
-            for arg in required:
-                parser.add_argument(arg, help=get_desc(arg))
-
-            # if entry point was decorated with @with_env, add arguments
-            # to replace declared variables in env.yaml
-            if hasattr(entry, '_env_dict'):
-                _add_args_from_env_dict(parser, entry._env_dict)
-
-            args = parser.parse_args()
-
-            if args.log is not None:
-                logging.basicConfig(level=args.log)
-
-            # required by the function signature
-            kwargs = {key: getattr(args, key) for key in required}
-
-            # env and function defaults replaced
-            replaced = _args_to_replace_in_env(args)
-
-            # TODO: add a way of test this by the parameters it will use to
-            # call the function, have an aux function to get those then another
-            # to execute, test using the first one
-            obj = entry(**{**kwargs, **replaced})
-
-            print(getattr(obj, args.action)())
-
-            return obj
+        dag = _process_entry_point(parser, entry_point)
+        return dag
 
 
 def _flatten_dict(d, prefix=''):
