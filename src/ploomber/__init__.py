e8487e61bc1fb3e6ca98af7adb8d1190d0cca874
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-07-22 11:44:04 -0400

    Release 0.6.2

--- src/ploomber/__init__.py
+++ src/ploomber/__init__.py
@@ -8,7 +8,7 @@ from ploomber.placeholders.SourceLoader import SourceLoader
 from ploomber.jupyter import _load_jupyter_server_extension
 
 
-__version__ = '0.6.2dev'
+__version__ = '0.6.2'
 
 # Set default logging handler to avoid "No handler found" warnings.
 
