800d21da357083f82a7a482e524ad2f3c75e5481
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-07-25 11:03:22 -0400

    adds FrozenJSON tests

--- src/ploomber/clients/__init__.py
+++ src/ploomber/clients/__init__.py
@@ -1,7 +1,8 @@
 from ploomber.clients.Client import Client
-from ploomber.clients.db import (DBAPIClient, SQLAlchemyClient,
-                                 DrillClient)
+from ploomber.clients.db import DBAPIClient, SQLAlchemyClient
 from ploomber.clients.shell import ShellClient, RemoteShellClient
 
-__all__ = ['Client', 'DBAPIClient', 'SQLAlchemyClient', 'DrillClient',
-           'ShellClient', 'RemoteShellClient']
+__all__ = [
+    'Client', 'DBAPIClient', 'SQLAlchemyClient', 'DrillClient', 'ShellClient',
+    'RemoteShellClient'
+]
---
+++ tests/test_FrozenJSON.py
@@ -0,0 +1,34 @@
+import pytest
+from pathlib import Path
+from ploomber.env.FrozenJSON import FrozenJSON
+
+
+def test_init_from_dict():
+    d_raw = {'a': {'b': 1}}
+    d = FrozenJSON(d_raw)
+
+    assert isinstance(d.a, FrozenJSON)
+    assert d.a.b == 1
+    assert d['a']['b'] == 1
+    assert str(d) == str(d_raw)
+
+
+def test_init_from_yaml(tmp_directory):
+    Path('some.yaml').write_text('a:\n  b: 1')
+
+    d = FrozenJSON.from_yaml('some.yaml')
+
+    assert isinstance(d.a, FrozenJSON)
+    assert d.a.b == 1
+    assert d['a']['b'] == 1
+    assert str(d) == 'some.yaml'
+
+
+def test_raises_key_error():
+    d = FrozenJSON({'a': 1})
+
+    with pytest.raises(KeyError):
+        d.b
+
+    with pytest.raises(KeyError):
+        d['b']
