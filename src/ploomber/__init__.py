3a885a995289871c2d99749b4e9f2dbc37a56a2f
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-08-19 18:30:48 -0400

    Bumps up project to version 0.7.4dev

--- src/ploomber/__init__.py
+++ src/ploomber/__init__.py
@@ -6,7 +6,7 @@ from ploomber.env.decorators import load_env, with_env
 from ploomber.placeholders.SourceLoader import SourceLoader
 from ploomber.jupyter import _load_jupyter_server_extension
 
-__version__ = '0.7.3'
+__version__ = '0.7.4dev'
 
 # Set default logging handler to avoid "No handler found" warnings.
 
