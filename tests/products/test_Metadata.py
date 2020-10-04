e064b3e0759321ab18b7a938496ec4edc0e89062
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-10-04 14:26:47 -0400

    improves metadata tests

--- tests/products/test_Metadata.py
+++ tests/products/test_Metadata.py
@@ -1,9 +1,31 @@
 from unittest.mock import Mock
+
+import pytest
+
 from ploomber.products.Metadata import Metadata
+from ploomber.products import Product
+
+
+class FakeProduct(Product):
+    def _init_identifier(self, identifier):
+        return identifier
+
+    def fetch_metadata(self):
+        pass
+
+    def save_metadata(self, metadata):
+        pass
+
+    def exists(self):
+        pass
+
+    def delete(self, force=False):
+        pass
 
 
 def test_clear():
-    prod = Mock()
+    prod = Mock(wraps=FakeProduct(identifier='fake-product'))
+    # we need this because if it doesn't exist, fetch_metata is skipped
     prod.exists.return_value = True
     metadata = Metadata(prod)
 
@@ -20,8 +42,9 @@ def test_clear():
     assert prod.fetch_metadata.call_count == 2
 
 
+@pytest.mark.xfail(reason='delete_metadata is not part of the abstract class')
 def test_delete():
-    prod = Mock()
+    prod = Mock(wraps=FakeProduct(identifier='fake-product'))
     metadata = Metadata(prod)
     assert not prod.delete_metadata.call_count
 
@@ -31,20 +54,28 @@ def test_delete():
 
 
 def test_update():
-    prod = Mock()
-    prod._outdated_data_dependencies_status = 1
-    prod._outdated_code_dependency_status = 1
+    prod = FakeProduct(identifier='fake-product')
 
-    # FIXME: delete once we get rid of this
-    prod.prepare_metadata = lambda product, metadata: None
-    prod.fetch_metadata.return_value = dict(timestamp=1,
-                                            stored_source_code='code')
     metadata = Metadata(prod)
 
     metadata.update('new code')
 
     # check code was updated
     assert metadata._data['stored_source_code'] == 'new code'
+
+
+@pytest.mark.parametrize(
+    'method, kwargs',
+    [['clear', dict()], ['update', dict(source_code='')],
+     ['update_locally', dict(data=dict())]])
+def test_cache_flags_are_cleared_up(method, kwargs):
+    prod = FakeProduct(identifier='fake-product')
+    prod._outdated_data_dependencies_status = 1
+    prod._outdated_code_dependency_status = 1
+
+    metadata = Metadata(prod)
+    getattr(metadata, method)(**kwargs)
+
     # check cache flags were cleared up
     assert prod._outdated_data_dependencies_status is None
     assert prod._outdated_code_dependency_status is None
