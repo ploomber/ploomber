ce7f100bfccdc60908303fe653f62c07cb6c739a
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-10-18 13:12:45 -0400

    updates paths to examples in the projects repo

--- doc/hooks.py
+++ doc/hooks.py
@@ -11,18 +11,18 @@ import nbformat
 
 
 def add_links(nb, name):
-    source = f'**Note:** This page was generated form a Jupyter notebook. [Click here](https://github.com/ploomber/projects/tree/master/{name}/nb.md) to see the source code. [Or here](https://mybinder.org/v2/gh/ploomber/projects/master?filepath={name}%2Fnb.md) to launch an interactive demo.'
+    source = f'**Note:** To see the source code of this page, [click here](https://github.com/ploomber/projects/tree/master/{name}/README.md). [Or here](https://mybinder.org/v2/gh/ploomber/projects/master?filepath={name}%2FREADME.md) to launch an interactive demo.'
     version = nbformat.versions[nb.nbformat]
     cell = version.new_markdown_cell(source=source)
     nb.cells.insert(0, cell)
 
 
 def process_tutorial(name):
-    nb = jupytext.read(f'projects-master/{name}/nb.md')
+    nb = jupytext.read(f'projects-master/{name}/README.md')
 
     add_links(nb, name)
 
-    jupytext.write(nb, f'projects-master/{name}/nb.ipynb')
+    jupytext.write(nb, f'projects-master/{name}/README.ipynb')
 
     if Path(f'projects-master/{name}/setup/script.sh').exists():
         subprocess.run(f'cd projects-master/{name}/setup && bash script.sh',
@@ -33,12 +33,12 @@ def process_tutorial(name):
                        shell=True,
                        check=True)
 
-    papermill.execute_notebook(f'projects-master/{name}/nb.ipynb',
-                               f'projects-master/{name}/nb.ipynb',
+    papermill.execute_notebook(f'projects-master/{name}/README.ipynb',
+                               f'projects-master/{name}/README.ipynb',
                                kernel_name='python3',
                                cwd=f'projects-master/{name}')
 
-    shutil.copy(f'projects-master/{name}/nb.ipynb',
+    shutil.copy(f'projects-master/{name}/README.ipynb',
                 f'../user-guide/{name}.ipynb')
 
 
