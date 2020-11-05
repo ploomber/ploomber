910d9b0d9b5364ddbc2c5ea0e806be03a29fdc12
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-11-05 00:45:32 -0500

    moving import

--- src/ploomber/cli/build.py
+++ src/ploomber/cli/build.py
@@ -1,11 +1,6 @@
 from functools import partial
 import sys
 
-# NOTE: the IPython.terminal.debugger module has pdb-like classes but it
-# doesn't mimic pdb's API exactly, ipdb is just a wrapper that takes care
-# of those details - I tried using IPython directly but bumped into some issues
-import ipdb
-
 from ploomber.cli.parsers import _custom_command, CustomParser
 from ploomber.executors import Serial
 
@@ -52,6 +47,8 @@ def main(render_only=False):
         else:
             callable_ = partial(dag.build, force=args.force)
 
+        raise Exception
+
         if args.debug:
             report = debug_mode(callable_)
         else:
@@ -64,6 +61,16 @@ def main(render_only=False):
 
 
 def debug_mode(callable_):
+    # NOTE: importing it here, otherwise we get a
+    # "If you suspect this is an IPython X.Y.Z bug..." message if any exception
+    # after the import if an exception happens
+    # NOTE: the IPython.terminal.debugger module has pdb-like classes but it
+    # doesn't mimic pdb's API exactly, ipdb is just a wrapper that takes care
+    # of those details - I tried using IPython directly but bumped into some
+    # issues
+    # should we implement in DAG, so we can do dag.build(debug=True)?
+    import ipdb
+
     try:
         report = callable_()
     except Exception:
