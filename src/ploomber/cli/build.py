c779facca8071ec221bc3681ba5f70d6f75cecfe
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-11-05 00:25:39 -0500

    adds --debug/-d option to ploomber build to drop a debugger if an exception happens

--- src/ploomber/cli/build.py
+++ src/ploomber/cli/build.py
@@ -1,4 +1,13 @@
+from functools import partial
+import sys
+
+# NOTE: the IPython.terminal.debugger module has pdb-like classes but it
+# doesn't mimic pdb's API exactly, ipdb is just a wrapper that takes care
+# of those details - I tried using IPython directly but bumped into some issues
+import ipdb
+
 from ploomber.cli.parsers import _custom_command, CustomParser
+from ploomber.executors import Serial
 
 
 # this parameter is only set to True when calling "ploomber interactive"
@@ -16,17 +25,50 @@ def main(render_only=False):
             '-p',
             help='Build a pipeline partially until certain task',
             default=None)
+        parser.add_argument(
+            '--debug',
+            '-d',
+            help='Drop a debugger session if an exception happens',
+            action='store_true',
+            default=False)
 
     dag, args = _custom_command(parser)
+
+    # if debug, we have to change the executor to these settings, if we run
+    # tasks in a subprocess or catch exception, we won't be able to start the
+    # debugging session in the right place
+    if args.debug:
+        dag.executor = Serial(build_in_subprocess=False,
+                              catch_exceptions=False,
+                              catch_warnings=False)
 
     if render_only:
         dag.render()
     else:
         if args.partially:
-            report = dag.build_partially(args.partially, force=args.force)
+            callable_ = partial(dag.build_partially,
+                                args.partially,
+                                force=args.force)
         else:
-            report = dag.build(force=args.force)
+            callable_ = partial(dag.build, force=args.force)
 
-        print(report)
+        if args.debug:
+            report = debug_mode(callable_)
+        else:
+            report = callable_()
+
+        if report:
+            print(report)
 
     return dag
+
+
+def debug_mode(callable_):
+    try:
+        report = callable_()
+    except Exception:
+        ipdb.post_mortem(sys.exc_info()[2])
+    finally:
+        report = None
+
+    return report
