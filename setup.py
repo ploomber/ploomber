5e6d29db5155801720357758a1de18c46615bb24
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-11-11 10:53:02 -0500

    disabling pygraphviz installation from setup.py

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ TESTING = [
     # plotting. strictly speaking pygrapviz is an optional dependency but we
     # don't add it as such because it's gonna break installation for most
     # setups, since we don't expect users to have graphviz installed
-    'pygraphviz',
+    # 'pygraphviz',
     # RemoteShellClient
     'paramiko',
     # Upload to S3
