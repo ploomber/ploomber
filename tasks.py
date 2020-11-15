38d3f872ce8c8080c1e8472a841d2cc93c30df24
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-11-14 22:28:45 -0500

    ignoring tests that run examples in mac os ci, adds a few missing parts to the setup command

--- tasks.py
+++ tasks.py
@@ -17,9 +17,14 @@ def db_credentials(c):
 
 @task
 def setup(c):
+    """
+    Setup dev environment, requires conda
+    """
     c.run('conda create --name ploomber python=3.8 --yes')
     c.run('eval "$(conda shell.bash hook)" '
-          '&& conda install graphviz r-base r-irkernel --yes'
+          '&& conda install pygraphviz r-base r-irkernel --yes '
+          '--channel conda-forge'
           '&& conda activate ploomber '
-          '&& pip install --editable .[all]')
+          '&& pip install --editable .[test] '
+          '&& bash install_test_pkg.sh')
     print('Done! Activate your environment with:\nconda activate ploomber')
