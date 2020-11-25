66b2a168ef6496e06f79acff00a5918081b45bca
Eduardo Blancas Reyes <github@blancas.io>
2020-11-25 10:55:30 -0500

    simplifies code that runs notebooks

--- doc/hooks.py
+++ doc/hooks.py
@@ -1,36 +1,19 @@
 from ploomberutils.nb import process_readme_md
-import os
 from pathlib import Path
 import shutil
 from urllib import request
 import zipfile
 
-# def add_links(nb, name):
-#     source = f'**Note:** To see the source code of this page, [click here](https://github.com/ploomber/projects/tree/master/{name}/README.md). [Or here](https://mybinder.org/v2/gh/ploomber/projects/master?filepath={name}%2FREADME.md) to launch an interactive demo.'
-#     version = nbformat.versions[nb.nbformat]
-#     cell = version.new_markdown_cell(source=source)
-#     nb.cells.insert(0, cell)
-
 
 def config_init(app, config):
-    tmp = Path('../projects-tmp/')
+    projects = Path('../../projects-ploomber/')
 
-    if tmp.exists():
-        return
+    if Path(projects).exists():
+        print('Using local copy...')
 
-    tmp.mkdir()
-
-    os.chdir('../projects-tmp')
-
-    path = '../../projects-ploomber'
-
-    if Path(path).exists():
-        print('Local copy...')
-        # create a symlink instead...
-        shutil.copytree(path, 'projects-master')
     else:
         print('Cloning from git...')
-        git_clone()
+        projects = Path('projects-master')
 
     directories = {
         'parametrized': 'user-guide',
@@ -40,13 +23,11 @@ def config_init(app, config):
         'spec-api-python': 'get-started'
     }
 
-    os.chdir('projects-master')
-
     process_readme_md(list(directories))
 
     for name, target_dir in directories.items():
-        src = f'{name}/README.ipynb'
-        dst = f'../../doc/{target_dir}/{name}.ipynb'
+        src = Path(projects, name, 'README.ipynb')
+        dst = Path(target_dir, f'{name}.ipynb')
         print(f'Copying {src!r} to {dst!r}')
         shutil.copy(src, dst)
 
