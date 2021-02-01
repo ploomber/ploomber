72e5f048752c1f4d8f3505a944c9141396a9c499
Eduardo Blancas Reyes <github@blancas.io>
2021-02-01 10:14:50 -0500

    moving more testing files to their cirresponding modules

--- tests/test_infer.py
+++ tests/test_infer.py
@@ -21,6 +21,18 @@ def test_quoted_relation_w_schema_to_str():
                                   'table')) == '"schema"."data"')
 
 
+def test_detects_create_table_w_schema():
+    ct = infer.created_relations("CREATE TABLE my_schema.my_table")
+    assert len(ct) == 1 and ct[0] == infer.ParsedSQLRelation(
+        'my_schema', 'my_table', 'table')
+
+
+def test_detects_create_table_wo_schema():
+    ct = infer.created_relations("CREATE TABLE my_table")
+    assert len(ct) == 1 and ct[0] == infer.ParsedSQLRelation(
+        None, 'my_table', 'table')
+
+
 sql_long = """
 CREATE VIEW schema.name  AS (
     WITH step_a AS (
