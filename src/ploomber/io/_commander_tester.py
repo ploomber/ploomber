e209c6ca10f876990f0dd338ca9fd2b2ccec6100
Eduardo Blancas Reyes <github@blancas.io>
2021-05-31 12:05:35 -0500

    CommanderTester collects mocked calls

--- src/ploomber/io/_commander_tester.py
+++ src/ploomber/io/_commander_tester.py
@@ -5,9 +5,16 @@ class CommanderTester:
     def __init__(self, run=None, return_value=None):
         self._run = run or []
         self._return_value = return_value or dict()
+        self._calls = []
 
     def __call__(self, cmd):
+        self._calls.append(cmd)
+
         if cmd in self._run:
             return subprocess.check_call(cmd)
         elif cmd in self._return_value:
             return self._return_value[cmd]
+
+    @property
+    def calls(self):
+        return self._calls
