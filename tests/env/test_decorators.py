6fb9aa9c426c8f4760f057c67dfa042d4216493a
Wandinho Oliveira <inovaprog@gmail.com>
2021-10-17 17:01:52 -0300

    add tests to validation to args starting with 'env'

---
+++ tests/env/test_decorators.py
@@ -0,0 +1,42 @@
+from ploomber.env.decorators import with_env
+import pytest
+
+
+def test_one_arg_not_start_with_env():
+    @with_env({'a': 1})
+    def works(env, b):
+        return b
+
+    assert works(2) == (2)
+
+
+def test_many_args_not_start_with_env():
+    @with_env({'a': 1})
+    def works(env, b, c):
+        return b, c
+
+    assert works(2, 3) == (2, 3)
+
+
+def test_one_arg_starting_with_env():
+    with pytest.raises(RuntimeError):
+
+        @with_env({'a': 1})
+        def does_not_works(env, env_something):
+            return env_something
+
+
+def test_many_args_starting_with_env():
+    with pytest.raises(RuntimeError):
+
+        @with_env({'a': 1})
+        def does_not_works(env, env_a, env_b):
+            return env_a, env_b
+
+
+def test_cap_arg_starting_with_env():
+    with pytest.raises(RuntimeError):
+
+        @with_env({'a': 1})
+        def does_not_works(env, ENV_something):
+            return ENV_something
