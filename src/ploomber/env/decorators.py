ea8003c852bff0467b84ae8d23cb4410271cdfb2
Eduardo Blancas <edu.blancas@gmail.com>
2021-10-17 17:16:18 -0400

    Update decorators.py

--- src/ploomber/env/decorators.py
+++ src/ploomber/env/decorators.py
@@ -26,11 +26,11 @@ def _validate_and_modify_signature(fn):
                            'argument, which is required to use the '
                            '@with_env decorator'.format(fn.__name__))
 
-    for arg in list(sig.parameters.keys())[1:]:
-        if arg[:3].lower() == 'env':
-            raise RuntimeError('Function "{}" accept arguments'
+    for arg in sig.parameters.keys()[1:]:
+        if arg.lower().startswith('env'):
+            raise RuntimeError('Function "{}" has arguments'
                                'starting with "env". Only the'
-                               'first should start with "env"'.format(
+                               'first one should start with "env"'.format(
                                    fn.__name__))
 
     # https://www.python.org/dev/peps/pep-0362/#examples
