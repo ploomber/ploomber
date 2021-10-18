71323ac59eba9075672032e33042c83bb5bb0e2a
Harsh Malani <harsh13092001@gmail.com>
2021-10-18 17:30:16 +0530

    add: find cells from multiple tags

--- src/ploomber/sources/nb_utils.py
+++ src/ploomber/sources/nb_utils.py
@@ -1,12 +1,16 @@
-def find_cell_with_tag(nb, tag):
+def find_cell_with_tag(nb, *tag):
     """
-    Find a cell with a given tag, returns a cell, index tuple. Otherwise
-    (None, None)
+    The input will be a series of tags. Returns a cell, index tuple if 
+    any cell with any of these tags found in the notebook. Otherwise
+    (None,None)
     """
+    all_tags = list(
+        tag)  # take any number of tags in input and convert it to a list
     for i, c in enumerate(nb['cells']):
         cell_tags = c['metadata'].get('tags')
         if cell_tags:
-            if tag in cell_tags:
-                return c, i
+            for tag_ in all_tags:  # take 1 tag from the list and find the cells with this tag
+                if tag_ in cell_tags:
+                    return c, i
 
     return None, None
