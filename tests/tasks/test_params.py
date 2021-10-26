3cca5297902f1e1298b2989152fd93045c3271a4
Eduardo Blancas Reyes <github@blancas.io>
2021-10-26 00:01:06 -0400

    fixes test

--- tests/tasks/test_params.py
+++ tests/tasks/test_params.py
@@ -15,7 +15,7 @@ def test_cannot_init_with_product_key():
     with pytest.raises(ValueError) as excinfo:
         Params({'product': None})
 
-    msg = ('Task params cannot be initialized with an '
+    msg = ('Task params cannot be initialized with a '
            '"product" key as it automatically added upon rendering')
     assert str(excinfo.value) == msg
 
