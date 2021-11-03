e33df6add8be64305d1e03cf3f781d957ee8f191
Harsh Malani <58773999+Harsh-1309@users.noreply.github.com>
2021-11-04 02:17:17 +0530

    add: find cells from multiple tags (#385)
    * add: find cells from multiple tags
    
    * fix finding cells with multiple tags

--- src/ploomber/sources/nb_utils.py
+++ src/ploomber/sources/nb_utils.py
@@ -1,12 +1,25 @@
-def find_cell_with_tag(nb, tag):
+def find_cell_with_tag(nb, *tag):
     """
-    Find a cell with a given tag, returns a cell, index tuple. Otherwise
-    (None, None)
+    The input will be a series of tags. Returns a dictionary which 
+    has "tag":index pairs. If no cells are found, then empty dict 
+    will be returned.
     """
-    for i, c in enumerate(nb['cells']):
-        cell_tags = c['metadata'].get('tags')
-        if cell_tags:
-            if tag in cell_tags:
-                return c, i
+    # take any number of tags in input and convert it to a list
+    all_tags = list(tag)
 
-    return None, None
+    #find all the tags in the given nb and add to dict with index
+    tags_found = {}
+    for index, cell in enumerate(nb['cells']):
+        temp = {}
+        cell_tags = cell['metadata'].get('tags')
+        temp[cell_tags] = index
+        tags_found.update(temp)
+
+    #dict for storing "tag":index pairs
+    result = {}
+    for tag_ in all_tags:
+        if tag_ in tags_found:
+            # if a cell is found with required tag, add the "tag":index to result
+            result[tag_] = tags_found[tag_]
+
+    return result
