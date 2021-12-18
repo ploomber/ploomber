0d47f91f29850de049f65749d03b21836db59d43
Eduardo Blancas Reyes <github@blancas.io>
2021-12-18 10:03:57 -0600

    improves install/uninstall of git hooks, adds post-commit hook

--- src/ploomber/cli/nb.py
+++ src/ploomber/cli/nb.py
@@ -1,3 +1,9 @@
+import shutil
+from pathlib import Path
+import stat
+
+import click
+
 from ploomber.cli.parsers import CustomParser
 from ploomber.cli.io import cli_endpoint
 
@@ -15,6 +21,52 @@ def _call_in_source(dag, method_name, kwargs=None):
         method(**kwargs)
 
 
+def _install_hook(path_to_hook, content):
+    if path_to_hook.exists():
+        raise RuntimeError(
+            'hook already exists '
+            f'at {path_to_hook}. Run: "ploomber nb -u" to uninstall the '
+            'existing hook and try again')
+
+    path_to_hook.write_text(content)
+    # make the file executable
+    path_to_hook.chmod(path_to_hook.stat().st_mode | stat.S_IEXEC)
+
+
+def _delete_hook(path):
+    if path.exists():
+        if path.is_file():
+            path.unlink()
+        else:
+            # in the remote case that it's a directory
+            shutil.rmtree(path)
+
+    click.echo(f'Deleted hook located at {path}')
+
+
+pre_commit_hook = """
+# !/usr/bin/env bash
+# Automatically generated pre-commit hook to remove the injected cell in
+# scripts and notebook tasks
+
+# remove injected cells
+ploomber nb --remove
+
+# re-add files
+git add $(git diff --name-only --cached)
+"""
+
+post_commit_hook = """
+# !/usr/bin/env bash
+# Automatically generated post-commit hook to add the injected cell in
+# scripts and notebook tasks
+
+# inject cells
+ploomber nb --inject
+"""
+
+
+# TODO: add unit tests
 @cli_endpoint
 def main():
     parser = CustomParser(description='Manage scripts and notebooks')
@@ -36,11 +88,29 @@ def main():
                             action='store_true',
                             help='Sync ipynb files')
 
-    dag, args = parser.load_from_entry_point_arg()
+        hook = parser.add_mutually_exclusive_group()
+        hook.add_argument('--install-hook',
+                          '-I',
+                          action='store_true',
+                          help='Install git pre-commit hook')
+        hook.add_argument('--uninstall-hook',
+                          '-u',
+                          action='store_true',
+                          help='Uninstall git pre-commit hook')
 
-    dag.render()
+    loading_error = None
 
-    # TODO: add --hook (pre-commit hook to remove cells)
+    try:
+        dag, args = parser.load_from_entry_point_arg()
+    except Exception as e:
+        loading_error = e
+    else:
+        dag.render()
+
+    # NOTE: what if the pipeline.yaml isn't in the same folder as .git,
+    # what if the project has multiple pipeline.yaml? - in such case, we may
+    # provide a feature to discover all of them, or the user could
+    # write the script
     # NOTE: maybe inject/remove should edit some project's metadata (not sure
     # if pipeline.yaml) or another file. so the Jupyter plug-in does not remove
     # the injected cell upon exiting
@@ -60,6 +130,34 @@ def main():
         _call_in_source(dag, 'sync')
 
     # can pair give trouble if we're reformatting?
-    elif args.pair:
+    if args.pair:
         # TODO: print a message suggesting to add the folder to .gitignore
         _call_in_source(dag, 'pair', dict(base_path=args.pair))
+
+    if args.install_hook:
+        # the generated hook should take into account the --entry-point arg
+        # check .git is in this folder
+
+        if not Path('.git').is_dir():
+            raise NotADirectoryError(
+                'Failed to install git hook: '
+                'expected a .git/ directory in the current working directory')
+
+        if loading_error:
+            raise RuntimeError('Could not install git hook: the DAG '
+                               'failed to load') from loading_error
+
+        parent = Path('.git', 'hooks')
+        parent.mkdir(exist_ok=True)
+
+        # pre-commit: remove injected cells
+        _install_hook(parent / 'pre-commit', pre_commit_hook)
+        click.echo('Successfully installed pre-commit git hook')
+
+        # post-commit: inject cells
+        _install_hook(parent / 'post-commit', post_commit_hook)
+        click.echo('Successfully installed post-commit git hook')
+
+    if args.uninstall_hook:
+        _delete_hook(Path('.git', 'hooks', 'pre-commit'))
+        _delete_hook(Path('.git', 'hooks', 'post-commit'))
