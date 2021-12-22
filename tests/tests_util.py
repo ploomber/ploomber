b3000993698ab7a51e7dde5b7d4b93aec7861129
Antonio Feregrino <antonio.feregrino@gmail.com>
2021-12-21 20:10:49 -0600

    Add tests for default pipeline.yaml

--- tests/tests_util.py
+++ tests/tests_util.py
@@ -1,6 +1,9 @@
 """
 This functions are imported in some tests
 """
+import ast
+from pathlib import Path
+
 from ploomber.executors import Serial
 from itertools import product
 
@@ -25,3 +28,22 @@ grid = {
 executors_w_exception_logging = [
     Serial(**kwargs) for kwargs in expand_grid(grid)
 ]
+
+
+def write_simple_pipeline(file_name, modules, function_name):
+    source = f"{'.'.join(modules)}.{function_name}"
+    Path(file_name).write_text(f"""
+tasks:
+    - source: {source}
+      product: out.ipynb
+""")
+
+
+def assert_function_in_module(function_name, module_file):
+    code = module_file.read_text()
+    module = ast.parse(code)
+    names = {
+        element.name
+        for element in module.body if hasattr(element, 'name')
+    }
+    assert function_name in names
