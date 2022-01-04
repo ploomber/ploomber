aef5c701495815f1d35bc171f843efe5ca61eb1e
Eduardo Blancas Reyes <github@blancas.io>
2022-01-03 23:16:17 -0600

    updates CommanderTester docstring

--- src/ploomber/io/_commander_tester.py
+++ src/ploomber/io/_commander_tester.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 class CommanderTester:
-    """Object to mock calls to subprocess.check_call
+    """Object to mock calls to ploomber.io._commander.Commander
 
     Parameters
     ----------
@@ -10,25 +10,25 @@ class CommanderTester:
         List of tuples, where each tuple represents the argument passed to
         subprocess.check_call. Whenever the mocked object calls, CommandTester
         checks if the passed arg is in the run list, if so, it will
-        call subprocess.check_call(arg)
+        call subprocess.{check_call, check_output} depending on the Commander's
+        capture_output argument value
 
     return_value : dict
         A mapping of tuples -> values. Whenever the mocked object calls,
         CommandTester checks if the passed arg is a key in this dictionary,
         if so, it returns the value corresponding to such key, instead
-        of calling subprocess.check_call
+        of calling subprocess.{check_call, check_output}
 
     Examples
     --------
-    >>> subprocess_mock = Mock()
+    >>> from ploomber.io import _commander
     >>> tester = CommanderTester(run=[('python', 'script.py')],
     ...                       return_value={('python', '--version'): b'3.10'})
+    >>> subprocess_mock = Mock()
+    >>> # we need to mock check_all and check_output since Commander uses both
     >>> subprocess_mock.check_call.side_effect = tester
-    >>> monkeypatch.setattr(some_module, 'subprocess', subprocess_mock)
-    >>> # performs the call to subprocess.check_call
-    >>> some_module.subprocess.check_call(['python', 'script.py'])
-    >>> # does not perform call to subprocess, it just returns b'3.10'
-    >>> some_module.subprocess.check_call(['python', '--version'])
+    >>> subprocess_mock.check_output.side_effect = tester
+    >>> monkeypatch.setattr(_commander, 'subprocess', subprocess_mock)
     """
     def __init__(self, run=None, return_value=None):
         self._run = run or []
