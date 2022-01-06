0b774b71305c19c80427a8533d1516afb1f32a0a
idomic <michael.ido@gmail.com>
2022-01-06 08:17:23 -0500

    CI Fixes

--- src/ploomber/telemetry/telemetry.py
+++ src/ploomber/telemetry/telemetry.py
@@ -41,7 +41,7 @@ from ploomber import __version__
 import urllib.request
 import platform
 
-TELEMETRY_VERSION = '0.1'
+TELEMETRY_VERSION = '0.2'
 DEFAULT_HOME_DIR = '~/.ploomber'
 CONF_DIR = "stats"
 posthog.project_api_key = 'phc_P9SpSeypyPwxrMdFn2edOOEooQioF2axppyEeDwtMSP'
