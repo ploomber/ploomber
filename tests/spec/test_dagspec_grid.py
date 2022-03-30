b1d96e571db0bb55b4b4170491c9d70f85b2c169
Eduardo Blancas Reyes <github@blancas.io>
2022-03-30 16:15:13 -0600

    closes #647

---
+++ tests/spec/test_dagspec_grid.py
@@ -0,0 +1,54 @@
+"""
+Tests for dagspecs using the grid feature
+"""
+from pathlib import Path
+
+import pytest
+import yaml
+from ploomber.spec import DAGSpec
+
+
+# NOTE: test_dagspec.py has the original tests for the grid feature but the
+# file is getting too large so we'll add new ones here. We need to move the
+# old ones to this file
+@pytest.fixture
+def sample_dagspec(tmp_directory):
+    spec = {
+        'tasks': [{
+            'source': 'upstream.py',
+            'name': 'upstream-',
+            'product': 'output/param=[[param]].ipynb',
+            'grid': {
+                'param': [1, 2]
+            }
+        }, {
+            'source': 'downstream.py',
+            'product': 'downstream.ipynb'
+        }]
+    }
+
+    Path('pipeline.yaml').write_text(yaml.dump(spec))
+
+    Path('upstream.py').write_text("""
+# + tags=['parameters']
+upstream = None
+param = None
+
+# +
+1 + 1
+""")
+
+    Path('downstream.py').write_text("""
+# + tags=['parameters']
+upstream = ['upstream-*']
+
+# +
+1 + 1
+""")
+
+
+def test_grid_with_params_placeholders(sample_dagspec):
+    dag = DAGSpec('pipeline.yaml').to_dag()
+
+    assert '/param=1-0.ipynb' in str(dag['upstream-0'].product)
+    assert '/param=2-1.ipynb' in str(dag['upstream-1'].product)
--- tests/tasks/test_taskgroup.py
+++ tests/tasks/test_taskgroup.py
@@ -38,6 +38,28 @@ def test_from_params():
     assert str(dag['task_group1'].product) == str(Path('dir', 'file-1.txt'))
 
 
+def test_from_params_with_product_placeholders():
+    dag = DAG()
+    group = TaskGroup.from_params(PythonCallable,
+                                  File,
+                                  'dir/param=[[param]].txt', {'source': touch},
+                                  dag,
+                                  name='task_group',
+                                  params_array=[{
+                                      'param': 1
+                                  }, {
+                                      'param': 2
+                                  }])
+
+    dag.render()
+
+    assert len(group) == 2
+    assert dag['task_group0'].source.primitive is touch
+    assert dag['task_group1'].source.primitive is touch
+    assert str(dag['task_group0'].product) == str(Path('dir', 'param=1-0.txt'))
+    assert str(dag['task_group1'].product) == str(Path('dir', 'param=2-1.txt'))
+
+
 @pytest.mark.parametrize('hook_name', ['on_render', 'on_finish', 'on_failure'])
 def test_from_params_with_hook(hook_name):
     def my_hook():
@@ -209,6 +231,28 @@ def test_metaproduct():
     assert len(group) == 2
 
 
+def test_metaproduct_with_product_placeholders():
+    dag = DAG()
+    group = TaskGroup.from_params(PythonCallable,
+                                  File, {
+                                      'one': 'param=[[param]].txt',
+                                      'another': 'another.txt'
+                                  }, {'source': touch},
+                                  dag,
+                                  name='task_group',
+                                  params_array=[{
+                                      'param': 1
+                                  }, {
+                                      'param': 2
+                                  }])
+
+    assert str(dag['task_group0'].product['one']) == 'param=1-0.txt'
+    assert str(dag['task_group0'].product['another']) == 'another-0.txt'
+    assert str(dag['task_group1'].product['one']) == 'param=2-1.txt'
+    assert str(dag['task_group1'].product['another']) == 'another-1.txt'
+    assert len(group) == 2
+
+
 def test_from_params_resolves_paths_in_metaproduct(tmp_directory):
     def touch(product, param):
         Path(product['one']).touch()
@@ -260,6 +304,25 @@ def test_sql_product():
     assert (id_.schema, id_.name, id_.kind) == ('schema', 'one-0', 'table')
     id_ = dag['task_group1'].product
     assert (id_.schema, id_.name, id_.kind) == ('schema', 'one-1', 'table')
+
+
+def test_sql_product_with_product_placeholders():
+    dag = DAG()
+    TaskGroup.from_params(PythonCallable,
+                          SQLRelation, ['schema', 'param=[[param]]', 'table'],
+                          {'source': touch},
+                          dag=dag,
+                          name='task_group',
+                          params_array=[{
+                              'param': 1
+                          }, {
+                              'param': 2
+                          }])
+
+    id_ = dag['task_group0'].product
+    assert (id_.schema, id_.name, id_.kind) == ('schema', 'param=1-0', 'table')
+    id_ = dag['task_group1'].product
+    assert (id_.schema, id_.name, id_.kind) == ('schema', 'param=2-1', 'table')
 
 
 def test_sql_meta_product():
