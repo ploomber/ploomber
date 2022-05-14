de2a454002e77f0039ba0e40501375b39d647709
Eduardo Blancas Reyes <github@blancas.io>
2022-05-13 22:16:13 -0500

    improved errors when network problems when calling cloud exec api

---
+++ tests/test_requests.py
@@ -0,0 +1,44 @@
+"""
+https://docs.python-requests.org/en/latest/api/#exceptions
+"""
+from unittest.mock import Mock
+
+import pytest
+import requests
+
+from ploomber import _requests, exceptions
+
+methods = [
+    _requests.get,
+    _requests.post,
+    _requests.delete,
+    _requests.put,
+]
+
+
+@pytest.fixture
+def mock_connection_error(monkeypatch):
+    for name in ('get', 'post', 'put', 'delete'):
+        monkeypatch.setattr(
+            _requests.requests, name,
+            Mock(side_effect=requests.exceptions.ConnectionError))
+
+
+@pytest.mark.parametrize('method', methods)
+def test_connection_error(method, mock_connection_error):
+    with pytest.raises(exceptions.NetworkException) as excinfo:
+        method('https://ploomber.io', json=dict(a=1))
+
+    assert 'connection error occurred' in str(excinfo.value)
+    assert 'https://ploomber.io' in str(excinfo.value)
+    assert 'https://ploomber.io/community' in str(excinfo.value)
+
+
+@pytest.mark.parametrize('method', methods)
+def test_timeout_error(method):
+    with pytest.raises(exceptions.NetworkException) as excinfo:
+        method('https://ploomber.io', timeout=0.01)
+
+    assert 'server took too long' in str(excinfo.value)
+    assert 'https://ploomber.io' in str(excinfo.value)
+    assert 'https://ploomber.io/community' in str(excinfo.value)
