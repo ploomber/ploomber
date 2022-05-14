f00bb5f6d46a25c823aea906ba70a322ad052334
Eduardo Blancas Reyes <github@blancas.io>
2022-05-13 22:16:13 -0500

    improved errors when network problems when calling cloud exec api

--- src/ploomber/_requests.py
+++ src/ploomber/_requests.py
@@ -4,27 +4,57 @@ module to provide friendlier error messages and defaults
 """
 import requests
 
-
-def _request_factory(method):
-    def _request(*args, **kwargs):
-        response = method(*args, **kwargs)
-
-        if response.status_code >= 300:
-            json_ = response.json()
-            message = json_.get("Message")
-
-            if message:
-                raise BaseException(
-                    f'{message} (status: {response.status_code})')
-            else:
-                raise BaseException(f'Error: {json_}')
-
-        return response
-
-    return _request
+from ploomber.exceptions import NetworkException
 
 
-get = _request_factory(requests.get)
-post = _request_factory(requests.post)
-put = _request_factory(requests.put)
-delete = _request_factory(requests.delete)
+def _make_error(reason):
+    return NetworkException(f'{reason}\n'
+                            'If you need help, reach out to us: '
+                            'https://ploomber.io/community')
+
+
+def _request(method, url, params=None, **kwargs):
+    try:
+        response = method(url, params=params, **kwargs)
+    except requests.exceptions.Timeout:
+        # we do this instead of a chained exception since we want to hide
+        # the original error and replace it with this one
+        error = _make_error('The server took too long to respond '
+                            f'when calling: {url}. Try again. ')
+    except requests.exceptions.ConnectionError:
+        error = _make_error('A connection '
+                            f'error occurred when calling: {url}. '
+                            'Verify your internet connection. ')
+    else:
+        error = None
+
+    if error:
+        raise error
+
+    if response.status_code >= 300:
+        json_ = response.json()
+        message = json_.get("Message")
+
+        if message:
+            raise NetworkException(
+                f'{message} (status: {response.status_code})')
+        else:
+            raise NetworkException(f'Error: {json_}')
+
+    return response
+
+
+def get(*args, **kwargs):
+    return _request(requests.get, *args, **kwargs)
+
+
+def post(*args, **kwargs):
+    return _request(requests.post, *args, **kwargs)
+
+
+def put(*args, **kwargs):
+    return _request(requests.put, *args, **kwargs)
+
+
+def delete(*args, **kwargs):
+    return _request(requests.delete, *args, **kwargs)
