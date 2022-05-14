58304ef1e65e99fdbdf9593170338ecef9643a0d
Eduardo Blancas Reyes <github@blancas.io>
2022-05-14 12:19:36 -0500

    fixes doctest run

--- conftest.py
+++ conftest.py
@@ -1,7 +1,13 @@
 """
-Configuration file for doctests
+Configuration file for doctests (under src/ploomber)
 """
-from tests.conftest import fixture_tmp_dir, _path_to_tests
+from pathlib import Path
+import sys
+
+path_to_src = str(Path(__file__, '..', 'src').resolve())
+sys.path.insert(0, path_to_src)
+
+from tests.conftest import fixture_tmp_dir, _path_to_tests  # noqa: E402
 
 
 @fixture_tmp_dir(_path_to_tests() / 'assets' / 'doctests', autouse=True)
