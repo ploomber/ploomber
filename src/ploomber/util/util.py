e7bf541b3b365fbaf9cb7fad35cebb581682f104
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-05-17 10:48:27 +0530

    few changes

--- src/ploomber/util/util.py
+++ src/ploomber/util/util.py
@@ -243,18 +243,18 @@ def signature_check(fn, params, task_name):
         msg = '. '.join(errors)
         # not all functions have __name__ (e.g. partials)
         fn_name = getattr(fn, '__name__', fn)
-        ele = missing.pop()
-        if(dl.get_close_matches(
-            ele, ['product'], cutoff=0.1, n=1
-        ) == ['product']):
-            raise TaskRenderError(
-                'You are passing {}. Do you mean {}?'.format(ele, "product")
-            )
-        else:
-            raise TaskRenderError(
-                'Error rendering task "{}" initialized with '
-                    'function "{}". {}'.format(
-                        task_name, fn_name, msg))
+        for e in missing:
+            if(dl.get_close_matches(
+                e, ['product'], cutoff=0.1, n=1
+            ) == ['product']):
+                raise TaskRenderError(
+                    'You are passing {}. Do you mean {}?'.format(e, "product")
+                )
+            else:
+                raise TaskRenderError(
+                    'Error rendering task "{}" initialized with '
+                        'function "{}". {}'.format(
+                            task_name, fn_name, msg))
 
     return True
 
