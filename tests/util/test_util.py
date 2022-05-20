b515fb722d1c472426c0eebcdcc0652aa8e70324
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-05-20 21:15:39 +0530

    pythonCallableSource

--- tests/util/test_util.py
+++ tests/util/test_util.py
@@ -3,10 +3,14 @@ import pytest
 from sklearn import datasets
 from pathlib import Path
 import pandas as pd
+from ploomber import dag
 from ploomber.exceptions import TaskRenderError
+from ploomber.sources.pythoncallablesource import PythonCallableSource
+from ploomber.spec.dagspec import DAGSpec
 
 from ploomber.util.util import signature_check
 
+'''
 def get(products):
     """Get data
     """
@@ -19,12 +23,22 @@ def get(products):
     Path(str(product)).parent.mkdir(exist_ok=True, parents=True)
 
     df.to_parquet(str(product))
+'''
 
 def test_signature_check():
+    
+    spec1 = {
+        'tasks': [{
+            'source': 'tasks.get',
+            'products': 'output/get.parquet',
+        }]
+    }
+
     error = ("You are passing products. Do you mean product?")
 
-    with pytest.raises(TaskRenderError) as e:
-        signature_check(get, {'product': 1}, 'get')
-        
-    assert error == str(e.value)
+    x = PythonCallableSource(str(spec1))
+    PythonCallableSource._post_render_validation(x, None, {'products': 1})
+    
+    #signature_check(get, {'product': 1}, 'get')      
+    #assert error == str(e.value)
     
