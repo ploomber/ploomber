65403f7574c33369870c17b42bc1b505e7878cec
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-05-20 21:15:07 +0530

    pythonCallableSource

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,7 +15,6 @@ import os
 import pytest
 from pathlib import Path
 import tempfile
-import test_pkg
 from ploomber.clients import SQLAlchemyClient
 from ploomber import Env
 import pandas as pd
@@ -81,25 +80,6 @@ def path_to_tests():
 @pytest.fixture()
 def path_to_test_pkg():
     return str(Path(importlib.util.find_spec('test_pkg').origin).parent)
-
-
-@pytest.fixture
-def backup_test_pkg():
-    old = os.getcwd()
-    backup = tempfile.mkdtemp()
-    root = Path(test_pkg.__file__).parents[2]
-
-    # sanity check, in case we change the structure
-    assert root.name == 'test_pkg'
-
-    shutil.copytree(str(root), str(Path(backup, 'test_pkg')))
-
-    yield str(Path(importlib.util.find_spec('test_pkg').origin).parent)
-    os.chdir(old)
-
-    shutil.rmtree(str(root))
-    shutil.copytree(str(Path(backup, 'test_pkg')), str(root))
-    shutil.rmtree(backup)
 
 
 @fixture_backup('spec-with-functions')
