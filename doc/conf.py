a0845f3e1a94fec079707b49e634ece04f2136b4
Eduardo Blancas Reyes <github@blancas.io>
2022-05-21 20:51:09 -0500

    adds sphinx-togglebutton

--- doc/conf.py
+++ doc/conf.py
@@ -36,6 +36,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.autosectionlabel',
     'nbsphinx',
+    'sphinx_togglebutton',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
