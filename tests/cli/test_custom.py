af2398e92e04d213fbc3f38f85c633522e82a7c6
idomic <michael.ido@gmail.com>
2022-05-31 09:25:35 -0400

    Fixing failing tests

--- tests/cli/test_custom.py
+++ tests/cli/test_custom.py
@@ -155,7 +155,7 @@ def test_status(monkeypatch, tmp_sample_dir):
     status.main(catch_exception=False)
 
 
-@pytest.mark.skipif(sys.version_info < (3, 10), reason="requires < python3.10")
+@pytest.mark.skipif(sys.version_info == (3, 10), reason="requires < 3.10")
 @pytest.mark.parametrize('custom_args, output, include_products, backend', [
     [[], 'pipeline.entry.png', False, None],
     [['--output', 'custom.png'], 'custom.png', False, None],
@@ -183,7 +183,7 @@ def test_plot(custom_args, monkeypatch, tmp_sample_dir, output,
                                  include_products=include_products)
 
 
-@pytest.mark.skipif(sys.version_info < (3, 10), reason="requires < python3.10")
+@pytest.mark.skipif(sys.version_info == (3, 10), reason="requires < 3.10")
 def test_plot_uses_name_if_any(tmp_nbs, monkeypatch):
     os.rename('pipeline.yaml', 'pipeline.train.yaml')
 
