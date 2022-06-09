c7f882515fba12958b4ac07b5109b05807e23b8c
shuyang <94rain@msn.com>
2022-06-09 01:18:19 +0000

    not include pygraphviz in testing setup

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ TESTING = [
     # plotting. strictly speaking pygrapviz is an optional dependency but we
     # don't add it as such because it's gonna break installation for most
     # setups, since we don't expect users to have graphviz installed
-    'pygraphviz',
+    'pygraphviz; python_version != "3.10"',
     # RemoteShellClient
     'paramiko',
     # Upload to S3
