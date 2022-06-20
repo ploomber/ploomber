b4b6c56b6952f255a7c4a51805b4b3bdbc7b8142
Neelasha Sen <neelasha.sen@gmail.com>
2022-06-20 13:07:51 +0530

    unlink

--- tests/products/test_file.py
+++ tests/products/test_file.py
@@ -308,8 +308,8 @@ def _edit_source_code(path):
 
 
 def _delete_metadata(path):
-    # Path(path).unlink()
-    os.remove(Path(path))
+    Path(path).unlink()
+    # os.remove(Path(path))
 
 
 @pytest.mark.parametrize('operation', [_edit_source_code, _delete_metadata])
