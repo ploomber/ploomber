850cebb19e8c5ca452318f9df526845fb132057d
shuyang <21193371+94rain@users.noreply.github.com>
2022-07-19 11:31:30 -0500

    Added D3 multiple backend support (#904)
    * add --backend to report command
    
    * update --backend help msg
    
    * Add initial report command tests
    
    * add backend arg to dag.plot
    
    * try using `plot.choose_backend(backend)`
    
    * add `dot -c` to win CIs
    
    * update help msg and remove backend in cli msg
    
    * remove backend assertion
    
    * address feedback

--- src/ploomber/dag/dag.py
+++ src/ploomber/dag/dag.py
@@ -809,12 +809,14 @@ class DAG(AbstractDAG):
         else:
             status = False
 
+        backend = plot.choose_backend(backend)
+
         if 'plot' in sections:
-            ext = '.png' if plot.choose_backend(backend) == 'pygraphviz' \
+            ext = '.png' if backend == 'pygraphviz' \
                   else '.html'
             fd, path_to_plot = tempfile.mkstemp(suffix=ext)
             os.close(fd)
-            self.plot(output=path_to_plot)
+            self.plot(output=path_to_plot, backend=backend)
             plot_ = image_bytes2html(Path(path_to_plot).read_bytes())
         else:
             plot_ = False
