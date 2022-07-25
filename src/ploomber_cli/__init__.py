39792ce65672efbd9f597ed97af2384ccfab6946
shuyang <21193371+94rain@users.noreply.github.com>
2022-07-25 16:34:13 +0000

    update deprecation warning message

--- src/ploomber_cli/__init__.py
+++ src/ploomber_cli/__init__.py
@@ -2,5 +2,6 @@ import warnings
 from sys import version_info
 if version_info < (3, 7):
     warnings.warn('Ploomber 0.20 will no longer be supported in Python 3.6.\n'
-                  'Please either downgrade ploomber or '
+                  'Please either downgrade ploomber by running '
+                  '`pip install ploomber==0.19` or '
                   'upgrade your Python version to 3.7+.')
