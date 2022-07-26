76199ede3b8ae66a422222de4e82e96a616e8060
shuyang <21193371+94rain@users.noreply.github.com>
2022-07-26 14:38:48 +0000

    Remove Python 3.6 deprecation warning

--- src/ploomber/__init__.py
+++ src/ploomber/__init__.py
@@ -1,13 +1,4 @@
 import logging
-import warnings
-from sys import version_info
-
-if version_info < (3, 7):
-    warnings.warn('Ploomber 0.20 will not support Python 3.6.\n'
-                  'Please either downgrade ploomber by running '
-                  '`pip install ploomber==0.19` or '
-                  'upgrade your Python version to 3.7+.')
-
 from ploomber.dag.dag import DAG
 from ploomber.dag.dagconfigurator import DAGConfigurator
 from ploomber.dag.inmemorydag import InMemoryDAG
