a2051ed0d8189a2fb1d3e1008de3e02210789ad8
Xilin <35501989+Wxl19980214@users.noreply.github.com>
2022-08-01 12:44:36 -0700

    unit tests for onboarding tutorial (#949)
    * adding unit tests for onboarding tutorial
    
    * add few unit tests
    
    * mock email_input
    
    * mock input function as well
    
    * mock in onboard
    
    * adding a return val for magic_mock
    
    * try skip python 3.7

---
+++ tests/onboard/test_onboard.py
@@ -0,0 +1,105 @@
+import subprocess
+from unittest.mock import Mock, MagicMock
+import click
+import sys
+import pytest
+from pathlib import Path
+from typing import Callable
+
+from ploomber.onboard import __main__ as onboard
+
+from ploomber.cli import examples
+from ploomber import DAG
+
+
+def test_make_open_callable():
+    callable_open = onboard._make_open_callable('open')
+    callable_xdg = onboard._make_open_callable('xdg-open')
+
+    assert isinstance(callable_open, Callable)
+    assert isinstance(callable_xdg, Callable)
+
+
+def test_echo_with_confirm(monkeypatch, capsys):
+    magic_mock = MagicMock()
+    monkeypatch.setattr(click, 'prompt', magic_mock)
+    onboard._echo('mock message', confirm=True)
+    captured = capsys.readouterr()
+
+    assert 'continue' not in captured.out
+    assert 'mock message' in captured.out
+
+
+def test_echo_without_confirm(capsys):
+    onboard._echo('mock message', confirm=False)
+    captured = capsys.readouterr()
+
+    assert '\nmock message' in captured.out
+    assert 'continue' not in captured.out
+
+
+def test_try_open_success(monkeypatch, tmp_directory, capsys):
+    mock = Mock()
+    monkeypatch.setattr(subprocess, 'run', mock)
+    Path('pipeline.yaml').write_text('test')
+    onboard._try_open('pipeline.yaml')
+    captured = capsys.readouterr()
+
+    assert 'Opening' in captured.out
+
+
+@pytest.mark.xfail(sys.platform == 'win32',
+                   reason='_try_open needs to be fixed first')
+def test_try_open_with_message(monkeypatch, tmp_directory, capsys):
+    mock = Mock()
+    monkeypatch.setattr(subprocess, 'run', mock)
+    path = Path('pipeline.yaml')
+    onboard._try_open_with_message(path)
+    captured = capsys.readouterr()
+
+    assert 'Done' in captured.out
+    assert 'Opening' in captured.out
+
+
+def test_load_dag(tmp_nbs):
+    dag = onboard._load_dag(tmp_nbs)
+
+    assert 'load.py' in str(dag['load'].source.loc)
+    assert isinstance(dag, DAG)
+
+
+def test_cmd(capsys):
+    onboard._cmd('open')
+    captured = capsys.readouterr()
+
+    assert 'Running' in captured.out
+
+
+@pytest.fixture(scope='function')
+def clone_examples():
+    examples.main(name=None, force=True)
+
+
+@pytest.mark.skipif(sys.version_info < (3, 8),
+                    reason='magic mock support pathlib 3.8 or higher')
+def test_main(clone_examples, monkeypatch, capsys, tmp_directory):
+    magic_mock = MagicMock()
+    magic_mock_return = MagicMock(return_value=True)
+    monkeypatch.setattr(subprocess, 'run', magic_mock)
+    monkeypatch.setattr(click, 'prompt', magic_mock)
+    monkeypatch.setattr(onboard, '_load_dag', magic_mock)
+    monkeypatch.setattr(examples._ExamplesManager, 'download', magic_mock)
+    monkeypatch.setattr(Path, 'relative_to', magic_mock)
+    monkeypatch.setattr(onboard, '_modified_task', magic_mock_return)
+    monkeypatch.setattr(onboard, '_try_open', magic_mock)
+    monkeypatch.setattr(onboard, '_email_input', magic_mock)
+
+    onboard.main()
+    captured = capsys.readouterr()
+
+    assert "Ploomber allows you to write modular data pipeline" in captured.out
+    assert "Let's install the dependencies!" in captured.out
+    assert "Let's plot the pipeline" in captured.out
+    assert "You can execute the pipeline with one command" in captured.out
+    assert "Ploomber automatically generates reports" in captured.out
+    assert "Please join our community" in captured.out
