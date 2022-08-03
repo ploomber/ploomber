5d4b2ff13ea206ae82b4bccbfc57ba185842e2c4
Eduardo Blancas Reyes <github@blancas.io>
2022-08-03 11:08:23 -0500

    adds function to validate input arguments

---
+++ src/ploomber/io/_validate.py
@@ -0,0 +1,13 @@
+"""
+Utilities for validating user input
+"""
+from ploomber.io import pretty_print
+
+
+def is_in(value, options, name):
+    if value not in options:
+        raise ValueError(
+            f"{value!r} is an invalid value for {name!r}. "
+            f"Valid values: {pretty_print.iterable(options, repr_=True)}")
+
+    return value
