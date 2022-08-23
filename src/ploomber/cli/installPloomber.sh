23730d40d0bfcd56d3402b5207ab635ec8dc4c82
e1ha <haelaine6@gmail.com>
2022-08-23 11:17:55 -0700

    Added a shell script for installing python and python script for installing ploomber

---
+++ src/ploomber/cli/installPloomber.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+which python > /dev/null 2>&1
+EXIT_STATUS=$?
+if [ "$EXIT_STATUS" -ne "0" ] 
+then
+    echo 'Python not found. Installing Python...'
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install python@3.9
+fi
+
+python ploomber/src/ploomber/cli/installPloomber.py
