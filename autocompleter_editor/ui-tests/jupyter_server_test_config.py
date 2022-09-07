58a6c0c6edc4b6c33b65e5137dc827e63d47d93b
e1ha <haelaine6@gmail.com>
2022-09-07 15:40:45 -0700

    Adjusted the #noqa

--- autocompleter_editor/ui-tests/jupyter_server_test_config.py
+++ autocompleter_editor/ui-tests/jupyter_server_test_config.py
@@ -4,18 +4,17 @@
 opens the server to the world and provide access to JupyterLab
 JavaScript objects through the global window variable.
 """
-#noqa
 from tempfile import mkdtemp
 
-c.ServerApp.port = 8888
-c.ServerApp.port_retries = 0
-c.ServerApp.open_browser = False
+c.ServerApp.port = 8888 #noqa
+c.ServerApp.port_retries = 0 #noqa
+c.ServerApp.open_browser = False #noqa
 
-c.ServerApp.root_dir = mkdtemp(prefix='galata-test-')
-c.ServerApp.token = ""
-c.ServerApp.password = ""
-c.ServerApp.disable_check_xsrf = True
-c.LabApp.expose_app_in_browser = True
+c.ServerApp.root_dir = mkdtemp(prefix='galata-test-') #noqa
+c.ServerApp.token = "" #noqa
+c.ServerApp.password = "" #noqa
+c.ServerApp.disable_check_xsrf = True #noqa
+c.LabApp.expose_app_in_browser = True #noqa
 
 # Uncomment to set server log level to debug level
 # c.ServerApp.log_level = "DEBUG"
