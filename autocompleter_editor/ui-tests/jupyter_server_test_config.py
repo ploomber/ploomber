b945feca719f6e5c2c661d95b3a7bb66477f3249
e1ha <haelaine6@gmail.com>
2022-09-08 08:59:22 -0700

    flake8 errors

--- autocompleter_editor/ui-tests/jupyter_server_test_config.py
+++ autocompleter_editor/ui-tests/jupyter_server_test_config.py
@@ -6,15 +6,15 @@ JavaScript objects through the global window variable.
 """
 from tempfile import mkdtemp
 
-c.ServerApp.port = 8888 #noqa
-c.ServerApp.port_retries = 0 #noqa
-c.ServerApp.open_browser = False #noqa
+c.ServerApp.port = 8888  # noqa
+c.ServerApp.port_retries = 0  # noqa
+c.ServerApp.open_browser = False  # noqa
 
-c.ServerApp.root_dir = mkdtemp(prefix='galata-test-') #noqa
-c.ServerApp.token = "" #noqa
-c.ServerApp.password = "" #noqa
-c.ServerApp.disable_check_xsrf = True #noqa
-c.LabApp.expose_app_in_browser = True #noqa
+c.ServerApp.root_dir = mkdtemp(prefix='galata-test-')  # noqa
+c.ServerApp.token = "" # noqa
+c.ServerApp.password = "" # noqa
+c.ServerApp.disable_check_xsrf = True # noqa
+c.LabApp.expose_app_in_browser = True # noqa
 
 # Uncomment to set server log level to debug level
 # c.ServerApp.log_level = "DEBUG"
