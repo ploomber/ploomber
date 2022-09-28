b9f8e97432926b2445dc1736b5327eb78bda1100
yafimvo <103026689+yafimvo@users.noreply.github.com>
2022-09-28 04:10:05 +0300

    switch embedded plot to use IFrame (#1017)
    * Change IPython display to use IFrame
    
    * Change the styling
    
    * Embedded file path fixed
    
    * embedded-assets dir fixed
    
    Co-authored-by: pelincetin <pc2807@columbia.edu>
    Co-authored-by: yafim <ifim.vo@gmail.com>

--- src/ploomber/dag/plot.py
+++ src/ploomber/dag/plot.py
@@ -1,10 +1,12 @@
+import os
 import json
 import sys
 from importlib.util import find_spec
 from pathlib import Path
-
 import jinja2
-from IPython.display import HTML
+from IPython.display import IFrame, HTML, display
+
+from ploomber.util.util import remove_dir
 
 try:
     import importlib.resources as importlib_resources
@@ -13,7 +15,6 @@ except ImportError:  # pragma: no cover
     import importlib_resources
 
 from ploomber import resources
-from ploomber.util.util import requires
 
 
 def check_pygraphviz_installed():
@@ -29,8 +30,7 @@ def choose_backend(backend):
        Temporarily disable pygraphviz for Python 3.10 on Windows
     """
     if ((not check_pygraphviz_installed() and backend is None)
-            or (backend == 'd3')
-            or (check_if_windows_python_3_10())):
+            or (backend == 'd3') or (check_if_windows_python_3_10())):
         return 'd3'
 
     return 'pygraphviz'
@@ -71,27 +71,28 @@ def with_d3(graph, output, image_only=False):
         Path(output).write_text(rendered)
 
 
-@requires(['requests_html', 'nest_asyncio'],
-          name='embedded HTML with D3 backend',
-          pip_names=['requests-html', 'nest_asyncio'])
 def embedded_html(path):
-    import asyncio
+    # set output cell to hold 100% of the embedded content
+    display(HTML('<style>.output.output_scroll { height: 100% }</style>'))
 
-    import nest_asyncio
-    nest_asyncio.apply()
-    return asyncio.get_event_loop().run_until_complete(
-        _embedded_html(path=path))
+    # create a copy of the file to embed in a local
+    # directory inorder to load it with iFrame
+
+    embedded_assets_dir = 'embedded-assets'
+    clear_embedded_assets_dir(embedded_assets_dir)
+
+    original_file = Path(path)
+    local_file_copy = os.path.join(embedded_assets_dir, original_file.name)
+
+    with open(local_file_copy, 'w+') as html:
+        copy_of_html = Path(path).read_text()
+        html.write(copy_of_html)
+
+    iframe = IFrame(src=local_file_copy, width='100%', height=600)
+
+    return iframe
 
 
-async def _embedded_html(path):
-    # https://github.com/jupyter/nbclient/blob/1d629b2bed561fde521e6408e190a8159f117ddc/nbclient/util.py
-    # https://github.com/jupyter/nbclient/blob/main/requirements.txt
-    from requests_html import HTML as HTML_
-    from requests_html import AsyncHTMLSession
-
-    session = AsyncHTMLSession()
-    html = HTML_(html=Path(path).read_text(), session=session)
-    await html.arender()
-    # ensure we close the session, otherwise this will fail on windows
-    await session.close()
-    return HTML(data=html.html)
+def clear_embedded_assets_dir(embedded_assets_dir):
+    remove_dir(embedded_assets_dir)
+    os.mkdir(embedded_assets_dir)
