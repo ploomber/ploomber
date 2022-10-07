d3e16d18edcc42faa195084e8d92cee26b0bfcd1
Prem Netsuwan <grnnja@gmail.com>
2022-10-07 13:57:34 -0700

    do not display warning when a task has multiple notebook outputs (#1021)
    * test multiple outputs in convert with kwargs not warning
    
    * fixes #980
    
    Co-authored-by: Eduardo Blancas Reyes <github@blancas.io>

--- tests/tasks/test_notebook.py
+++ tests/tasks/test_notebook.py
@@ -1,6 +1,7 @@
 import sys
 from pathlib import Path, PurePosixPath
 from unittest.mock import ANY, Mock
+import warnings
 
 import jupytext
 import nbconvert
@@ -488,6 +489,35 @@ Path(product['model']).touch()
     dag.build()
 
 
+def test_do_not_warn_on_nbconvert_export_kwargs_if_multiple_outputs(
+        tmp_directory):
+    dag = DAG()
+
+    code = """
+# + tags=["parameters"]
+upstream = None
+
+# +
+1 + 1
+    """
+
+    NotebookRunner(code,
+                   product={
+                       'nb': File('out.ipynb'),
+                       'report': File('out.html'),
+                   },
+                   dag=dag,
+                   ext_in='py',
+                   nb_product_key=['nb', 'report'],
+                   nbconvert_export_kwargs=dict(exclude_input=True),
+                   name='nb')
+
+    with warnings.catch_warnings():
+        # fail the test if this displays a warnings
+        warnings.simplefilter("error")
+        dag.build()
+
+
 @pytest.mark.parametrize('product, nb_product_key, nbconvert_exporter_name', [
     (
         {
@@ -523,7 +553,7 @@ Path(product['model']).touch()
         'file': File(Path('another', 'data', 'file.txt')),
     }, 'nb', 'webpdf')
 ])
-def test_multiple_nb_product_success(product, nb_product_key,
+def test_multiple_nb_product_success(tmp_directory, product, nb_product_key,
                                      nbconvert_exporter_name):
     dag = DAG()
 
