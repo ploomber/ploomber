37171c1bf1e37247bc46e40c01066e1eeb41100d
idomi <michael.ido@gmail.com>
2022-11-08 10:56:48 -0500

    revert

--- test/ml-online/clients.py
+++
@@ -1,6 +0,0 @@
-from ploomber.clients import S3Client
-
-def get_s3():
-    return S3Client(bucket_name='api-stack-dev-ploombercloudartifactsd7322d06-1bd3rk42l1ns5',
-                    parent='ml-online-output',
-                    json_credentials_path='credentials.json')
