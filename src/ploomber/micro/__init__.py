e9d22f205e6822e10b57d69a937efe394a8c58b2
Mohit Mehta <43580047+mehtamohit013@users.noreply.github.com>
2023-04-11 17:39:48 -0400

    fix tests, remove `capture` from `micro` API (#1093)

--- src/ploomber/micro/__init__.py
+++ src/ploomber/micro/__init__.py
@@ -2,6 +2,6 @@
 A module for defining micro pipelines. You can use it to define and run
 pipelines inside a Jupyter notebook
 """
-from ploomber.micro._micro import dag_from_functions, grid, capture
+from ploomber.micro._micro import dag_from_functions, grid
 
-__all__ = ['dag_from_functions', 'grid', 'capture']
+__all__ = ["dag_from_functions", "grid"]
