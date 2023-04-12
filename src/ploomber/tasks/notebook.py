e8485e18f144f49ba43e6fed170e0bd9adce67e3
Mohit Mehta <mehtamohit013@gmail.com>
2023-04-12 13:21:27 -0400

    Added Documentation

--- src/ploomber/tasks/notebook.py
+++ src/ploomber/tasks/notebook.py
@@ -823,6 +823,38 @@ class ScriptRunner(NotebookMixin, Task):
     it also works by injecting a cell into the source code. Source can be
     a ``.py`` script or an ``.ipynb`` notebook. **Does not support magics.**
 
+    Parameters
+    ----------
+    source: str or pathlib.Path
+        Script source, if str, the content is interpreted as the actual
+        script, if pathlib.Path, the content of the file is loaded. When
+        loading from a str, ext_in must be passed
+    product: ploomber.File
+        The output file
+    dag: ploomber.DAG
+        A DAG to add this task to
+    name: str, optional
+        A str to indentify this task. Should not already exist in the dag
+    params: dict, optional
+        Script parameters. This are passed as the "parameters" argument
+        to the papermill.execute_notebook function, by default, "product"
+        and "upstream" are included
+    ext_in: str, optional
+        Source extension. Required if loading from a str. If source is a
+        ``pathlib.Path``, the extension from the file is used.
+    static_analysis : ('disabled', 'regular', 'strict'), default='regular'
+        Check for various errors in the script. In 'regular' mode, it aborts
+        execution if the notebook has syntax issues, or similar problems that
+        would cause the code to break if executed. In 'strict' mode, it
+        performs the same checks but raises an issue before starting execution
+        of any task, furthermore, it verifies that the parameters cell and
+        the params passed to the notebook match, thus, making the script
+        behave like a function with a signature.
+    local_execution : bool, optional
+        Change working directory to be the parent of the script's source.
+        Defaults to True. This resembles the default behavior when
+        running script from the parent folder
+
     Examples
     --------
 
