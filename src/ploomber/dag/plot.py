121400445f4b046941285b10c39e4c7ff13d9443
tomarm <a@a.com>
2023-07-28 22:29:04 +1200

    added mermaid plot

--- src/ploomber/dag/plot.py
+++ src/ploomber/dag/plot.py
@@ -32,7 +32,10 @@ def choose_backend(backend, path=None):
         or (backend is None and path and Path(path).suffix == ".html")
     ):
         return "d3"
-
+    
+    elif (backend == "mermaid"):
+        return "mermaid"
+    
     return "pygraphviz"
 
 
@@ -66,6 +69,50 @@ def with_d3(graph, output, image_only=False):
         )
 
         rendered = template.render(json_data=json_data)
+        Path(output).write_text(rendered)
+
+def with_mermaid(graph, output, image_only=False):
+    """Generate mermaid diagram of DAG"""
+
+    indent = "    "
+
+    # Make an empty array of the lines of the mermaid diagram.
+    # we will push nodes / links to this array and then flatten it at the end.
+    diagram_markup_lines = []
+
+    # If there are a large number of nodes in the DAG, make a vertical diagram
+    diagram_direction = "LR" if len(graph["nodes"]) < 10 else "TD"
+
+    diagram_markup_lines.append(f"graph {diagram_direction}")
+
+    def pascall_case(s):
+        p = ''.join(x for x in s.title().replace("-"," ") if not x.isspace())
+        return p
+
+
+    # Add the nodes to the diagram
+    for node in graph['nodes']:
+        node_id = pascall_case(node['id'])
+        node_name = node['id']
+        diagram_markup_lines.append(f"{indent}{node_id}[{node_name}]")
+
+    # Add the links
+    for link in graph['links']:
+        target = pascall_case(link['target'])
+        source = pascall_case(link['source'])
+        diagram_markup_lines.append(f"{indent}{source} --> {target}")
+
+    # Flatten array to markup string
+    diagram_markup = "\n".join(diagram_markup_lines)
+
+    if image_only:
+        raise NotImplementedError("Mermaid back end supports html output only")
+    else:
+        template = jinja2.Template(
+            importlib_resources.read_text(resources, "dag_template_mermaid.html")
+        )
+
+        rendered = template.render(diagram_markup=diagram_markup)
         Path(output).write_text(rendered)
 
 
