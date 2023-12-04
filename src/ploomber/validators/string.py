8fadadd572ec63a76a325388d5af6a7893af7372
matt.barr <matt@mysidewalk.com>
2023-12-03 18:26:20 -0600

    Added dynamic importing of fully qualified task and product classes.

--- src/ploomber/validators/string.py
+++ src/ploomber/validators/string.py
@@ -37,21 +37,54 @@ def str_to_class(key_str):
     return _KEY2CLASS.get(key_str, None)
 
 
+def _import_class(dotted_path, base_class=None):
+    """
+    Import a class from a dotted path string. Optionally, check if the class
+    is a subclass of a specified base class.
+    """
+    module_path, class_name = dotted_path.rsplit(".", 1)
+    module = importlib.import_module(module_path)
+    klass = getattr(module, class_name)
+    # Ensure klass is a type
+    if not isinstance(klass, type):
+        raise TypeError(f"{klass} is not a class")
+    # If a base class is provided, check if klass is a subclass of it
+    if base_class is not None:
+        if not issubclass(klass, base_class):
+            raise TypeError(f"{klass} is not a subclass of {base_class}")
+    return klass
+
+
+def _validate_class(value, base_class, key2class, normalized):
+    """
+    Validates if a string is a valid class name (e.g., SQLScipt).
+    Raises a ValueError if not.
+    """
+    # Dotted path to class
+    if "." in value:
+        return _import_class(value, base_class=base_class)
+    # Ploomber class
+    if value in key2class:
+        return key2class[value]
+    else:
+        suggestion = get_suggestion(value, mapping=normalized)
+        msg = f"{value!r} is not a valid class name"
+        if suggestion:
+            msg += f". Did you mean {suggestion!r}?"
+        raise ValueError(msg)
+
+
 def validate_task_class_name(value):
     """
     Validates if a string is a valid Task class name (e.g., SQLScipt).
     Raises a ValueError if not.
     """
-    if value not in _KEY2CLASS_TASKS:
-        suggestion = get_suggestion(value, mapping=_NORMALIZED_TASKS)
-        msg = f"{value!r} is not a valid Task class name"
-
-        if suggestion:
-            msg += f". Did you mean {suggestion!r}?"
-
-        raise ValueError(msg)
-
-    return _KEY2CLASS_TASKS[value]
+    return _validate_class(
+        value,
+        base_class=tasks.Task,
+        key2class=_KEY2CLASS_TASKS,
+        normalized=_NORMALIZED_TASKS,
+    )
 
 
 def validate_product_class_name(value):
@@ -59,13 +92,9 @@ def validate_product_class_name(value):
     Validates if a string is a valid Product class name (e.g., File).
     Raises a ValueError if not.
     """
-    if value not in _KEY2CLASS_PRODUCTS:
-        suggestion = get_suggestion(value, mapping=_NORMALIZED_PRODUCTS)
-        msg = f"{value!r} is not a valid Product class name"
-
-        if suggestion:
-            msg += f". Did you mean {suggestion!r}?"
-
-        raise ValueError(msg)
-
-    return _KEY2CLASS_PRODUCTS[value]
+    return _validate_class(
+        value,
+        base_class=products.Product,
+        key2class=_KEY2CLASS_PRODUCTS,
+        normalized=_NORMALIZED_PRODUCTS,
+    )
